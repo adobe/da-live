bc3927b85a9b043e8ae8ee32ab27f7315cd7f0f3
Chris Millar <cmillar@adobe.com>
2023-04-15 22:32:45 -0600

    Server origin

--- blocks/browse/state/index.js
+++ blocks/browse/state/index.js
@@ -1,7 +1,7 @@
 import { getLibs } from '../../../scripts/utils.js';
 const { signal } = await import(`${getLibs()}/deps/htm-preact.js`);
 
-export const origin = signal('http://localhost:8787');
+export const origin = signal('https://das.chris4303.workers.dev');
 export const breadcrumbs = signal([]);
 export const content = signal([]);
 export const showFolder = signal(false);
