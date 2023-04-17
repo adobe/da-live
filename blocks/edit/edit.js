b86997aeeb26266f51e20e34f3dc868dc3d5f894
Chris Millar <cmillar@adobe.com>
2023-04-16 22:49:11 -0600

    Somewhat of an editor

--- blocks/edit/edit.js
+++ blocks/edit/edit.js
@@ -1,6 +1,30 @@
+import { origin } from '../browse/state/index.js';
+import getTools from './tools/index.js';
+
 export default async function init(el) {
     const { getLibs } = await import('../../../scripts/utils.js');
     const { createTag } = await import(`${getLibs()}/utils/utils.js`);
 
-    el.append(createTag('h2', { class: 'da-edit-title' }, 'This does\'t exist yet.'));
+    const { hash } = window.location;
+    const filename = hash.split('/').pop();
+    const name = createTag('h1', { class: 'da-title-name' }, filename);
+
+    const title = createTag('div', { class: 'da-title' }, name);
+    const editor = createTag('div', { class: 'da-editor', contenteditable: true, id: 'da-editor' });
+
+    const meta = createTag('div', { class: 'da-meta' });
+
+    const tools = await getTools(el);
+
+    el.append(title, editor, meta, tools);
+
+    // const h2 = createTag('h2', { class: 'da-edit-title' }, 'This does\'t exist yet.');
+
+    // const headerOpts = { 'Content-Type': 'text/html' };
+    // const headers = new Headers(headerOpts);
+
+    // const blob = new Blob([h2.outerHTML], { type: 'text/html' });
+
+    // const opts = { method: 'PUT', headers, body: blob };
+    // const resp = await fetch(`${origin}/content/test.html`, opts);
 }
---
+++ blocks/edit/tools/index.js
@@ -0,0 +1,93 @@
+const { getLibs } = await import('../../../../scripts/utils.js');
+const { createTag } = await import(`${getLibs()}/utils/utils.js`);
+const { html, render } = await import(`${getLibs()}/deps/htm-preact.js`);
+
+function isInEditor(node) {
+  if (node?.id === 'da-editor') return true;
+  const closest = node?.parentElement.closest('#da-editor');
+  if (closest) return true;
+  return false;
+}
+
+function insert(dom) {
+  const sel = window.getSelection();
+  const inEditor = isInEditor(sel.anchorNode);
+  
+  if (inEditor) {
+    const range = sel.getRangeAt(0);
+    range.deleteContents();
+    range.insertNode(dom);
+  } else {
+    const editor = document.querySelector('#da-editor');
+    editor.append(dom);
+  }
+
+  dom.focus();
+}
+
+function makeUl() {
+  const dom = createTag('ul', null, '<li></li>');
+  insert(dom);
+}
+
+function makeOl() {
+  const dom = createTag('ol', null, '<li></li>');
+  insert(dom);
+}
+
+function makeBr() {
+  const dom = createTag('hr', null);
+  insert(dom);
+}
+
+function makeLink() {
+  const dom = createTag('a', { href: 'https://www.adobe.com' }, 'Adobe Inc');
+  insert(dom);
+}
+
+function makeBlock() {
+  const rows = '<tr><th colspan="2">block-name</th></tr><tr><td>contents 1</td><td>contents 2</td></tr>';
+  const dom = createTag('table', { href: 'https://www.adobe.com' }, rows);
+  insert(dom);
+}
+
+function Toolbar() {
+  return html`
+    <button
+      data-label="Link"
+      aria-label="Add link"
+      onClick=${makeLink}
+      class="da-editor-tool da-editor-tool-a"/>
+    <button
+      data-label="Unorderd list"
+      aria-label="Add unordered list"
+      onClick=${makeUl}
+      class="da-editor-tool da-editor-tool-ul"/>
+    <button
+      data-label="Orderd list"
+      aria-label="Add ordered list"
+      onClick=${makeOl}
+      class="da-editor-tool da-editor-tool-ol"/>
+    <button
+      data-label="Section break"
+      aria-label="Add section break"
+      onClick=${makeBr}
+      class="da-editor-tool da-editor-tool-hr"/>
+    <button
+      data-label="Block"
+      aria-label="Add empty block"
+      onClick=${makeBlock}
+      class="da-editor-tool da-editor-tool-block"/>
+    <hr/>
+    <button
+      data-label="Libraries"
+      aria-label="Open library"
+      class="da-editor-tool da-editor-tool-library"/>
+  `;
+}
+
+export default async function init() {
+  const el = createTag('div', { class: 'da-editor-tools' });
+  render(html`<${Toolbar} />`, el);
+  return el;
+}
