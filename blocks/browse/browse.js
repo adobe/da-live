62dd7a078731e3c81fe177cbc73cf8269ab4785f
Chris Millar <cmillar@adobe.com>
2023-12-03 00:03:31 -0700

    Basic browsing

--- blocks/browse/browse.js
+++ blocks/browse/browse.js
@@ -1,15 +1,26 @@
 import { getPathDetails } from '../edit/shared/utils.js';
 
 async function loadComponent(el, cmpName, details) {
+  el.innerHTML = '';
   await import(`./${cmpName}/${cmpName}.js`);
   const cmp = document.createElement(`${cmpName}`);
   if (details) cmp.details = details;
   el.append(cmp);
 }
 
-async function setupExperience(el) {
-  el.innerHTML = '';
+async function setupExperience(el, e) {
   const details = getPathDetails();
+  if (e) {
+    const oldHash = new URL(e.oldURL).hash;
+    const newHash = new URL(e.newURL).hash;
+
+    // Are they already browsing
+    if (oldHash.startsWith('#/') && newHash.startsWith('#/')) {
+      document.querySelector('da-browse').details = details;
+      return;
+    }
+  }
+  console.log('starting over');
   if (!details) {
     loadComponent(el, 'da-orgs');
   } else {
@@ -20,7 +31,7 @@ async function setupExperience(el) {
 export default async function init(el) {
   await setupExperience(el);
 
-  window.addEventListener('hashchange', async () => {
-    await setupExperience(el);
+  window.addEventListener('hashchange', async (e) => {
+    await setupExperience(el, e);
   });
 }
--- blocks/browse/da-browse/da-browse.js
+++ blocks/browse/da-browse/da-browse.js
@@ -1,11 +1,15 @@
 import { LitElement, html, map } from '../../../deps/lit/lit-all.min.js';
+import { origin } from '../state/index.js';
 
 import getSheet from '../../shared/sheet.js';
 const sheet = await getSheet('/blocks/browse/da-browse/da-browse.css');
 
 export default class DaBrowse extends LitElement {
   static properties = {
-    details: { attribute: false },
+    details: {
+      attribute: false,
+    },
+    _listItems: {  },
   };
 
   constructor() {
@@ -13,19 +17,43 @@ export default class DaBrowse extends LitElement {
   }
 
   async getList() {
-    const resp = await fetch(``)
+    const resp = await fetch(`${origin}/list${this.details.fullpath}`);
+    if (!resp.ok) return;
+    return resp.json();
+  }
+
+  getEditPath(path) {
+    const pathSplit = path.split('.');
+    // This will fail spectacularly if there are other periods in the path
+    return `/edit#${pathSplit[0]}`;
   }
 
   connectedCallback() {
     super.connectedCallback();
     this.shadowRoot.adoptedStyleSheets = [sheet];
-    this.getList();
+  }
+
+  async update(props) {
+    if (props.has('details')) {
+      this._listItems = await this.getList();
+    }
+    super.update(props);
   }
 
   render() {
     return html`
       <h1>Browse</h1>
-      
+      <ul class="da-item-list">
+        ${map(this._listItems, (item) => html`
+          <li class="da-item-list-item">
+            <input type="checkbox" name="select" style="display: none;">
+            <a href="${item.isFile ? this.getEditPath(item.path) : `/#${item.path}`}" class="da-item-list-item-title">
+              <span class="da-item-list-item-type ${item.isFile ? 'da-item-list-item-type-file' : 'da-item-list-item-type-folder' }">
+              </span>${item.name.split('.')[0]}
+            </a>
+          </li>
+        `)}
+      </ul>
     `;
   }
 }
