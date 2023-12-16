f5ebf0721ade4cc6f965c1c80b4f5a3a45705253
Chris Millar <cmillar@adobe.com>
2023-12-15 20:20:09 -0700

    Auth testing

---
+++ blocks/auth-test/auth-test.js
@@ -0,0 +1,24 @@
+import { origin } from '../browse/state/index.js';
+
+// Milo Imports
+const { getLibs } = await import('../../scripts/utils.js');
+const { loadIms } = await import(`${getLibs()}/utils/utils.js`);
+
+async function imsReady() {
+  const accessToken = window.adobeIMS.getAccessToken();
+
+  const headers = {};
+
+  if (accessToken) {
+    headers.Authorization = `Bearer ${accessToken.token}`;
+  }
+
+  const resp = await fetch(`${origin}/list/hlxsites`, { headers });
+  if (!resp.ok) return;
+  const json = await resp.json();
+  console.log(json);
+}
+
+export default function init(el) {
+  loadIms().then(() => { imsReady(); });
+}
