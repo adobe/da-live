8d1cf5cc42887bea52de62afcbef915c48683102
Karl Pauls <karlpauls@gmail.com>
2024-02-13 00:37:46 +0100

    Don't request content from da-admin

--- blocks/edit/da-editor/da-editor.js
+++ blocks/edit/da-editor/da-editor.js
@@ -1,47 +1,43 @@
 import { LitElement, html } from '../../../deps/lit/lit-core.min.js';
-import getContent from '../utils/content.js';
 import initProse from '../prose/index.js';
 import getSheet from '../../shared/sheet.js';
 
 const sheet = await getSheet('/blocks/edit/da-editor/da-editor.css');
 
+// Milo Imports
+const { getLibs } = await import('../../../scripts/utils.js');
+const { loadIms } = await import(`${getLibs()}/utils/utils.js`);
+
 export default class DaEditor extends LitElement {
   static properties = {
     path: {},
-    _content: {},
-    _contentLoaded: { state: true },
+    _imsLoaded: { state: false },
   };
-
-  async getContent(path) {
-    this._contentLoaded = false;
-    this._content = await getContent(`${path}`);
-    this._contentLoaded = true;
-    this._path = path;
-  }
 
   connectedCallback() {
     super.connectedCallback();
     this.shadowRoot.adoptedStyleSheets = [sheet];
     this.shadowRoot.createRange = () => document.createRange();
+    loadIms().then(() => {
+      this._imsLoaded = true;
+    });
   }
 
   attributeChangedCallback(name, _old, value) {
     super.attributeChangedCallback();
     if (name !== 'path') return;
-    this.getContent(value);
+    this._path = value;
   }
 
   render() {
-    if (!this._content) return null;
+    if (!this._imsLoaded) return null;
     return html`<div class="da-prose-mirror"></div>`;
   }
 
   updated() {
-    if (!this._content) return;
+    if (!this._imsLoaded) return;
     const prose = this.shadowRoot.querySelector('.da-prose-mirror');
     prose.innerHTML = '';
-    const content = document.createElement('div');
-    content.append(...this._content);
     initProse({ editor: prose, path: this._path });
   }
 }
