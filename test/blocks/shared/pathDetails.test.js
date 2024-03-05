faacea52c1126a38786fa758a72610e340f58b3c
David Bosschaert <bosschae@adobe.com>
2024-03-05 16:36:45 +0000

    Back out pathDetails.test.js changes

--- test/blocks/shared/pathDetails.test.js
+++ test/blocks/shared/pathDetails.test.js
@@ -6,32 +6,32 @@ const { default: getPathDetails } = await import('../../../blocks/shared/pathDet
 
 describe('Path details', () => {
   describe('Full path details', () => {
-    it('HTML path details', () => {
+    describe('HTML path details', () => {
       const loc = { pathname: '/edit', hash: '#/adobe/aem-boilerplate/cool/page' };
       const details = getPathDetails(loc);
-      expect(details.origin).to.equal('https://admin.da.live');
+      expect(details.origin).to.equal('http://localhost:8787');
       expect(details.owner).to.equal('adobe');
       expect(details.repo).to.equal('aem-boilerplate');
       expect(details.fullpath).to.equal('/adobe/aem-boilerplate/cool/page');
-      expect(details.sourceUrl).to.equal('https://admin.da.live/source/adobe/aem-boilerplate/cool/page.html');
+      expect(details.sourceUrl).to.equal('http://localhost:8787/source/adobe/aem-boilerplate/cool/page.html');
       expect(details.contentUrl).to.equal('https://content.da.live/adobe/aem-boilerplate/cool/page');
       expect(details.previewUrl).to.equal('https://main--aem-boilerplate--adobe.hlx.page/cool/page');
     });
 
-    it('JSON path details', () => {
+    describe('JSON path details', () => {
       const loc = { pathname: '/sheet', hash: '#/adobe/aem-boilerplate/cool/data' };
       const details = getPathDetails(loc);
-      expect(details.sourceUrl).to.equal('https://admin.da.live/source/adobe/aem-boilerplate/cool/data.json');
+      expect(details.sourceUrl).to.equal('http://localhost:8787/source/adobe/aem-boilerplate/cool/data.json');
       expect(details.contentUrl).to.equal('https://content.da.live/adobe/aem-boilerplate/cool/data.json');
     });
 
-    it('JPG path details', () => {
+    describe('JPG path details', () => {
       const loc = { pathname: '/view', hash: '#/adobe/aem-boilerplate/cool/pic.jpg' };
       const details = getPathDetails(loc);
-      expect(details.sourceUrl).to.equal('https://admin.da.live/source/adobe/aem-boilerplate/cool/pic.jpg');
+      expect(details.sourceUrl).to.equal('http://localhost:8787/source/adobe/aem-boilerplate/cool/pic.jpg');
     });
 
-    it('Top level path details', () => {
+    describe('Top level path details', () => {
       const loc = { pathname: '/view', hash: '#/adobe/aem-boilerplate/pic.jpg' };
       const details = getPathDetails(loc);
       expect(details.parentName).to.equal('aem-boilerplate');
@@ -40,7 +40,7 @@ describe('Path details', () => {
   });
 
   describe('Repo only path details', () => {
-    it('Path details', () => {
+    describe('Path details', () => {
       const loc = { hash: '#/adobe/aem-boilerplate' };
       const details = getPathDetails(loc);
       expect(details.previewUrl).to.equal('https://main--aem-boilerplate--adobe.hlx.page');
@@ -48,7 +48,7 @@ describe('Path details', () => {
   });
 
   describe('Owner only path details', () => {
-    it('Path details', () => {
+    describe('Path details', () => {
       const loc = { hash: '#/adobe' };
       const details = getPathDetails(loc);
       expect(details.previewUrl).to.not.exist;
@@ -56,19 +56,17 @@ describe('Path details', () => {
   });
 
   describe('IMS callback path details', () => {
-    it('Path details', () => {
+    describe('Path details', () => {
       const loc = { hash: '#old_hash' };
       const details = getPathDetails(loc);
       expect(details).to.be.null;
     });
   });
 
-  /*
   describe('IMS callback path details', () => {
-    it('Path details', () => {
+    describe('Path details', () => {
       const details = getPathDetails();
       expect(details).to.be.null;
     });
   });
-  */
 });
