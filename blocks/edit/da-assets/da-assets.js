d6a61ae742fa46607341dd6767cef40d72d72404
Chris Millar <cmillar@adobe.com>
2024-03-24 16:17:16 -0600

    GH-90 - AEM Assets Integration
    * Allows admins to spec a cloud service repository per project
    * Shows a button to creators if assets is enabled.
    * Opens modal to let creator choose asset.
    * Automatically inserts asset into doc and maintains AEM origin.
    * Converts author URL to publish (polaris coming soon).
    
    Resolves: GH-90

---
+++ blocks/edit/da-assets/da-assets.js
@@ -0,0 +1,69 @@
+import { getNx } from '../../../scripts/utils.js';
+import getPathDetails from '../../shared/pathDetails.js';
+import { DA_ORIGIN } from '../../shared/constants.js';
+import { daFetch } from '../../shared/utils.js';
+
+const { loadScript, loadStyle } = await import(`${getNx()}/scripts/nexter.js`);
+const { loadIms, handleSignIn } = await import(`${getNx()}/utils/ims.js`);
+
+const ASSET_SELECTOR_URL = 'https://experience.adobe.com/solutions/CQ-assets-selectors/assets/resources/assets-selectors.js';
+
+export async function getRepoId() {
+  const { repo, owner } = getPathDetails();
+  if (!(repo || owner)) return null;
+  const resp = await daFetch(`${DA_ORIGIN}/config/${owner}/${repo}`);
+  if (!resp.ok) return null;
+  const json = await resp.json();
+  const repoConf = json.data.find((conf) => conf.key === 'aem.repositoryId');
+  if (!repoConf) return null;
+  return repoConf.value;
+}
+
+export async function openAssets() {
+  const repoId = await getRepoId();
+  const details = await loadIms();
+  if (details.anonymous) handleSignIn();
+  if (!(repoId && details.accessToken)) return;
+
+  let dialog = document.querySelector('.da-dialog-asset');
+  if (!dialog) {
+    await loadStyle(import.meta.url.replace('.js', '.css'));
+    await loadScript(ASSET_SELECTOR_URL);
+
+    dialog = document.createElement('dialog');
+    dialog.className = 'da-dialog-asset';
+
+    const inner = document.createElement('div');
+    inner.className = 'da-dialog-asset-inner';
+
+    dialog.append(inner);
+
+    const main = document.body.querySelector('main');
+    main.insertAdjacentElement('afterend', dialog);
+
+    const selectorProps = {
+      imsToken: details.accessToken.token,
+      repositoryId: repoId,
+      aemTierType: 'author',
+      onClose: () => { dialog.close(); },
+      handleSelection: (assets) => {
+        const [asset] = assets;
+        if (!asset) return;
+        const format = asset['aem:formatName'];
+        if (!format) return;
+        const { path } = asset;
+        const pubRepoId = repoId.replace('author', 'publish');
+        const { view } = window;
+        const { state } = view;
+        dialog.close();
+
+        const imgObj = { src: `https://${pubRepoId}${path}`, style: 'width: 180px' };
+        const fpo = state.schema.nodes.image.create(imgObj);
+        view.dispatch(state.tr.insert(state.selection.head, fpo).scrollIntoView());
+      },
+    };
+    window.PureJSSelectors.renderAssetSelector(inner, selectorProps);
+  }
+
+  dialog.showModal();
+}
