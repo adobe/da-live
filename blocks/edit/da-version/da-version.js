51a80471deca00e94fc85442fce48eb54e340cef
David Bosschaert <bosschae@adobe.com>
2024-03-25 12:09:36 +0000

    Add prosemirror styling to version view.

--- blocks/edit/da-version/da-version.js
+++ blocks/edit/da-version/da-version.js
@@ -1,12 +1,14 @@
 import { LitElement, html } from '../../../deps/lit/lit-core.min.js';
 import getSheet from '../../shared/sheet.js';
 
+// TODO we could split up the editor sheet into a shared prosemirror one instead
+const editorSheet = await getSheet('/blocks/edit/da-editor/da-editor.css');
 const sheet = await getSheet('/blocks/edit/da-version/da-version.css');
 
 export default class DaVersion extends LitElement {
   connectedCallback() {
     super.connectedCallback();
-    this.shadowRoot.adoptedStyleSheets = [sheet];
+    this.shadowRoot.adoptedStyleSheets = [editorSheet, sheet];
   }
 
   render() {
