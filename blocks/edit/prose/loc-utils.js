ecd651c2d25aef3bdcb879d65c928d75aa0beef6
ChrisChrisChris <chrischrischris@users.noreply.github.com>
2024-05-10 13:39:08 -0400

    Add e2e test; Code refactoring

--- blocks/edit/prose/loc-utils.js
+++ blocks/edit/prose/loc-utils.js
@@ -19,67 +19,78 @@ const LOC = {
   },
 };
 
+function getCoverDiv(isLangstore) {
+  const coverDiv = document.createElement('div');
+  coverDiv.className = `loc-color-overlay ${isLangstore ? 'loc-langstore' : 'loc-regional'}`;
+  coverDiv.setAttribute('loc-temp-dom', '');
+
+  coverDiv.style.backgroundColor = isLangstore
+    ? LOC.LANGSTORE.COVER_BG
+    : LOC.REGIONAL.COVER_BG;
+  return coverDiv;
+}
+
+function getLangOverlay(isLangstore) {
+  const overlay = document.createElement('div');
+  overlay.className = 'loc-lang-overlay';
+  overlay.setAttribute('loc-temp-dom', '');
+  overlay.style.backgroundColor = isLangstore
+    ? LOC.LANGSTORE.BG
+    : LOC.REGIONAL.BG;
+
+  const dialog = document.createElement('div');
+  dialog.className = 'loc-dialog';
+  dialog.innerHTML = `
+    <span>${isLangstore ? LOC.LANGSTORE.TEXT : LOC.REGIONAL.TEXT}</span>
+    <div>
+    <span class="loc-keep"><div title="Keep">Keep</div></span>
+    <span class="loc-delete"><div title="Delete">Delete</div></span>
+    </div>`;
+  dialog.style.color = isLangstore
+    ? LOC.LANGSTORE.TEXT_COLOR
+    : LOC.REGIONAL.TEXT_COLOR;
+
+  const deleteBtn = dialog.querySelector('.loc-delete');
+  const keepBtn = dialog.querySelector('.loc-keep');
+  overlay.appendChild(dialog);
+
+  return { overlay, deleteBtn, keepBtn };
+}
+
+function keepLocContentInPlace(view, pos, node) {
+  node.content.content = node.content.content.filter((c) => c.content.content.length);
+  const newFragment = Fragment.fromArray(node.content.content);
+  const newSlice = new Slice(newFragment, 0, 0);
+  const transaction = view.state.tr.replace(pos, pos + node.nodeSize, newSlice);
+  return transaction;
+}
+
+function deleteLocContent(view, pos, node) {
+  const resolvedPos = view.state.doc.resolve(pos);
+
+  if (resolvedPos.parent.type.name === 'list_item') {
+    const parentPos = resolvedPos.before(resolvedPos.depth);
+    const transaction = view.state.tr.delete(parentPos, parentPos + resolvedPos.parent.nodeSize);
+    return transaction;
+  }
+
+  const transaction = view.state.tr.delete(pos, pos + node.nodeSize);
+  return transaction;
+}
+
+function parseLocDOM(locTag) {
+  return [{
+    tag: locTag,
+    contentElement: (dom) => {
+      // Only parse the content of the node, not the temporary elements
+      const deleteThese = dom.querySelectorAll('[loc-temp-dom]');
+      deleteThese.forEach((e) => e.remove());
+      return dom;
+    },
+  }];
+}
+
 export function getLocClass(elName, getSchema, dispatchTransaction, { isLangstore } = {}) {
-  function getCoverDiv() {
-    const coverDiv = document.createElement('div');
-    coverDiv.className = `loc-color-overlay ${isLangstore ? 'loc-langstore' : 'loc-regional'}`;
-    coverDiv.setAttribute('loc-temp-dom', '');
-
-    coverDiv.style.backgroundColor = isLangstore
-      ? LOC.LANGSTORE.COVER_BG
-      : LOC.REGIONAL.COVER_BG;
-    return coverDiv;
-  }
-
-  function getLangOverlay() {
-    const overlay = document.createElement('div');
-    overlay.className = 'loc-lang-overlay';
-    overlay.setAttribute('loc-temp-dom', '');
-    overlay.style.backgroundColor = isLangstore
-      ? LOC.LANGSTORE.BG
-      : LOC.REGIONAL.BG;
-
-    const dialog = document.createElement('div');
-    dialog.className = 'loc-dialog';
-    dialog.innerHTML = `
-      <span>${isLangstore ? LOC.LANGSTORE.TEXT : LOC.REGIONAL.TEXT}</span>
-      <div>
-      <span class="loc-keep"><div title="Keep">Keep</div></span>
-      <span class="loc-delete"><div title="Delete">Delete</div></span>
-      </div>`;
-    dialog.style.color = isLangstore
-      ? LOC.LANGSTORE.TEXT_COLOR
-      : LOC.REGIONAL.TEXT_COLOR;
-
-    const deleteBtn = dialog.querySelector('.loc-delete');
-    const keepBtn = dialog.querySelector('.loc-keep');
-    overlay.appendChild(dialog);
-
-    return { overlay, deleteBtn, keepBtn };
-  }
-
-  function keepLocContentInPlace(view, pos, node) {
-    node.content.content = node.content.content.filter((c) => c.content.content.length);
-    const newFragment = Fragment.fromArray(node.content.content);
-    const newSlice = new Slice(newFragment, 0, 0);
-    const transaction = view.state.tr.replace(pos, pos + node.nodeSize, newSlice);
-    dispatchTransaction(transaction);
-  }
-
-  function deleteLocContent(view, pos, node) {
-    const resolvedPos = view.state.doc.resolve(pos);
-
-    if (resolvedPos.parent.type.name === 'list_item') {
-      const parentPos = resolvedPos.before(resolvedPos.depth);
-      // Create a transaction that deletes the parent node
-      const transaction = view.state.tr.delete(parentPos, parentPos + resolvedPos.parent.nodeSize);
-      dispatchTransaction(transaction);
-    } else {
-      const transaction = view.state.tr.delete(pos, pos + node.nodeSize);
-      dispatchTransaction(transaction);
-    }
-  }
-
   return class {
     constructor(node, view, getPos) {
       this.dom = document.createElement(elName);
@@ -87,17 +98,17 @@ export function getLocClass(elName, getSchema, dispatchTransaction, { isLangstor
       const nodeDOM = serializer.serializeFragment(node.content);
 
       this.dom.appendChild(nodeDOM);
-      const coverDiv = getCoverDiv();
+      const coverDiv = getCoverDiv(isLangstore);
       this.dom.appendChild(coverDiv);
-      const { overlay, deleteBtn, keepBtn } = getLangOverlay();
+      const { overlay, deleteBtn, keepBtn } = getLangOverlay(isLangstore);
       this.langOverlay = overlay;
 
       deleteBtn.addEventListener('click', () => {
-        deleteLocContent(view, getPos(), node);
+        dispatchTransaction(deleteLocContent(view, getPos(), node));
       });
 
       keepBtn.addEventListener('click', () => {
-        keepLocContentInPlace(view, getPos(), node);
+        dispatchTransaction(keepLocContentInPlace(view, getPos(), node));
       });
 
       coverDiv.appendChild(this.langOverlay);
@@ -118,18 +129,6 @@ export function getLocClass(elName, getSchema, dispatchTransaction, { isLangstor
 
     stopEvent() { return true; }
   };
-}
-
-function parseLocDOM(locTag) {
-  return [{
-    tag: locTag,
-    contentElement: (dom) => {
-      // Only parse the content of the node, not the temporary elements
-      const deleteThese = dom.querySelectorAll('[loc-temp-dom]');
-      deleteThese.forEach((e) => e.remove());
-      return dom;
-    },
-  }];
 }
 
 export function addLocNodes(baseNodes) {
