c79c8930a698233c81c2dca8e4c9cd18cce31b22
David Bosschaert <bosschae@adobe.com>
2024-05-13 15:49:48 +0100

    Fix comment

--- test/unit/blocks/edit/da-editor/da-editor.test.js
+++ test/unit/blocks/edit/da-editor/da-editor.test.js
@@ -1,7 +1,7 @@
 import { expect } from '@esm-bundle/chai';
 
 // This is needed to make a dynamic import work that is indirectly referenced
-// from edit/prose/index.js
+// from da-editor.js
 const { setNx } = await import('../../../../../scripts/utils.js');
 setNx('/bheuaark/', { hostname: 'localhost' });
 
