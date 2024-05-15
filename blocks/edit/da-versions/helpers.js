52f27d50b728401250f055dbe8cf660f68229c73
Chris Millar <cmillar@apache.org>
2024-05-15 03:27:40 -0600

    GH-108 - Versions & Audits (#129)
    * Provide history pane in editor
    * History pane shows both audits and versions
    * Audits are grouped by day
    * Audits show time & associated users
    * Versions show time, users, and optional label
    * Versions and audits of the same day are separate by design
    * Provide ability to expand each date to see time and users
    * Provide ability to click version icon to see a preview
    * Ability to restore a version
    * Ability to create a new labeled version
    
    Resolves: GH-108

---
+++ blocks/edit/da-versions/helpers.js
@@ -0,0 +1,41 @@
+export function formatDate(timestamp) {
+  const rawDate = timestamp ? new Date(timestamp) : new Date();
+  const date = rawDate.toLocaleDateString([], { year: 'numeric', month: 'long', day: 'numeric' });
+  const time = rawDate.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' });
+  return { date, time };
+}
+
+export function formatVersions(json) {
+  // Sort by timestamp epoch
+  json.sort((a, b) => b.timestamp - a.timestamp);
+
+  // Make human readable entries
+  const ungrouped = json.map((entry) => {
+    const { date, time } = formatDate(entry.timestamp);
+    return {
+      date,
+      time,
+      ...entry,
+      isVersion: !!entry.url,
+    };
+  });
+
+  // Group everything by date
+  return ungrouped.reduce((acc, entry) => {
+    // Elevate versions to the top
+    if (entry.isVersion) {
+      acc.push(entry);
+    } else {
+      // Group audits by date
+      const notVerSameDate = acc.find(
+        (accEntry) => !accEntry.isVersion && accEntry.date === entry.date,
+      );
+      if (!notVerSameDate) {
+        acc.push({ date: entry.date, audits: [entry] });
+      } else {
+        notVerSameDate.audits.push(entry);
+      }
+    }
+    return acc;
+  }, []);
+}
