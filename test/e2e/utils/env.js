242c050fcdef8cc036546f0880649bb8bbcb29bb
David Bosschaert <bosschae@adobe.com>
2024-05-22 09:18:34 +0100

    Fix derived branch name default. (#143)
    The previous default didn't work if the variable had an empty String as
    value.

--- test/e2e/utils/env.js
+++ test/e2e/utils/env.js
@@ -1,5 +1,8 @@
 function getEnv() {
-  const { GITHUB_HEAD_REF: branch = 'main' } = process.env;
+  let { GITHUB_HEAD_REF: branch } = process.env;
+  if (!branch) {
+    branch = 'main';
+  }
   return branch === 'local' ? 'http://localhost:3000' : `https://${branch}--da-live--adobe.hlx.live`;
 }
 
