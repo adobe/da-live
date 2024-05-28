e292e45c673f9fdbcff26bab72348204dcaa0af8
David Bosschaert <bosschae@adobe.com>
2024-05-28 14:11:39 +0100

    Allow local da-collab and da-admin for PW tests

--- test/e2e/utils/page.js
+++ test/e2e/utils/page.js
@@ -11,10 +11,20 @@
  */
 import ENV from './env.js';
 
+function getQuery() {
+  const { GITHUB_HEAD_REF: branch } = process.env;
+  if (branch === 'local') {
+    return '?da-admin=local&da-collab=local';
+  }
+  return '';
+}
+
+const QUERY = getQuery();
+
 function getTestURL(type, testIdentifier, workerInfo) {
   const dateStamp = Date.now().toString(36);
   const pageName = `pw-${testIdentifier}-${dateStamp}-${workerInfo.project.name}`;
-  return `${ENV}/${type}#/da-sites/da-status/tests/${pageName}`;
+  return `${ENV}/${type}${QUERY}#/da-sites/da-status/tests/${pageName}`;
 }
 
 /**
