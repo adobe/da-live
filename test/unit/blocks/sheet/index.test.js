e8280beb12f131585bbd00a1c9def93a837db368
Chris Peyer <chrischrischris@users.noreply.github.com>
2024-10-11 09:49:37 -0400

    #231 Private Sheet Support (#238)
    * #231 Private Sheet Support
    
    Private sheets must be prepended by `private-`
    
    This also saves the single sheet name instead of reverting to `data`.
    
    * Add tests

--- test/unit/blocks/sheet/index.test.js
+++ test/unit/blocks/sheet/index.test.js
@@ -90,4 +90,140 @@ describe('Sheets', () => {
       window.fetch = savedFetch;
     }
   });
+
+  it('Test private single sheet', async () => {
+    const json = `
+    {
+      "total": 1,
+      "limit": 1,
+      "offset": 0,
+      "data": [
+        {
+          "single": "1",
+          "sheet": "2",
+          "here": "3"
+        }
+      ],
+      ":colWidths": [10, 20, 30],
+      ":sheetname": "single-sheet",
+      ":type": "sheet",
+      ":private": {
+        "private-sheet": {
+          "total": 1,
+          "limit": 1,
+          "offset": 0,
+          "data": [
+            {
+              "private": "10",
+              "sheet": "20",
+              "here": "30"
+            }
+          ],
+          ":colWidths": [5, 10, 15]
+        }
+      }
+    }
+    `;
+
+    const mockFetch = async (url) => {
+      if (url === 'http://example.com') {
+        return new Response(json, { status: 200 });
+      }
+      return undefined;
+    };
+
+    const savedFetch = window.fetch;
+    try {
+      window.fetch = mockFetch;
+
+      const sheet = await sh.getData('http://example.com');
+      expect(sheet.length).to.equal(2);
+      expect(sheet[0].sheetName).to.equal('single-sheet');
+      expect(sheet[1].sheetName).to.equal('private-sheet');
+      expect(sheet[0].data).to.deep.equal([['single', 'sheet', 'here'], ['1', '2', '3']]);
+      expect(sheet[1].data).to.deep.equal([['private', 'sheet', 'here'], ['10', '20', '30']]);
+      expect(sheet[0].columns).to.deep.equal([{ width: '10' }, { width: '20' }, { width: '30' }]);
+      expect(sheet[1].columns).to.deep.equal([{ width: '5' }, { width: '10' }, { width: '15' }]);
+    } finally {
+      window.fetch = savedFetch;
+    }
+  });
+
+  it('Test private multi sheet', async () => {
+    const json = `
+      {
+        "sheet1": {
+          "total": 1,
+          "limit": 1,
+          "offset": 0,
+          "data": [
+            {
+              "sheet1": "1",
+              "stuff": "2"
+            }
+          ],
+          ":colWidths": [10, 20]
+        },
+        "sheet2": {
+          "total": 1,
+          "limit": 1,
+          "offset": 0,
+          "data": [
+            {
+              "sheet2": "2",
+              "hello": "world"
+            }
+          ],
+          ":colWidths": [30, 40]
+        },
+        ":names": ["sheet1", "sheet2"],
+        ":version": 3,
+        ":type": "multi-sheet",
+        ":private": {
+          "private-mysheet": {
+            "total": 1,
+            "limit": 1,
+            "offset": 0,
+            "data": [
+              {
+                "this": "1",
+                "is": "2",
+                "private": "3"
+              }
+            ],
+            ":colWidths": [50, 60, 70]
+          }
+        }
+      }
+
+    `;
+
+    const mockFetch = async (url) => {
+      if (url === 'http://example.com') {
+        return new Response(json, { status: 200 });
+      }
+      return undefined;
+    };
+
+    const savedFetch = window.fetch;
+    try {
+      window.fetch = mockFetch;
+
+      const sheet = await sh.getData('http://example.com');
+      expect(sheet.length).to.equal(3);
+      expect(sheet[0].sheetName).to.equal('sheet1');
+      expect(sheet[1].sheetName).to.equal('sheet2');
+      expect(sheet[2].sheetName).to.equal('private-mysheet');
+
+      expect(sheet[0].data).to.deep.equal([['sheet1', 'stuff'], ['1', '2']]);
+      expect(sheet[1].data).to.deep.equal([['sheet2', 'hello'], ['2', 'world']]);
+      expect(sheet[2].data).to.deep.equal([['this', 'is', 'private'], ['1', '2', '3']]);
+
+      expect(sheet[0].columns).to.deep.equal([{ width: '10' }, { width: '20' }]);
+      expect(sheet[1].columns).to.deep.equal([{ width: '30' }, { width: '40' }]);
+      expect(sheet[2].columns).to.deep.equal([{ width: '50' }, { width: '60' }, { width: '70' }]);
+    } finally {
+      window.fetch = savedFetch;
+    }
+  });
 });
