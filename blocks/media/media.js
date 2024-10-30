1c0d6afa6444e7c494d6e563126dfbb6c799366f
Chris Millar <cmillar@adobe.com>
2024-10-30 17:32:53 -0600

    Fix PDF Viewer SDK loading 401 files

--- blocks/media/media.js
+++ blocks/media/media.js
@@ -2,35 +2,62 @@ import getPathDetails from '../shared/pathDetails.js';
 import { getNx } from '../../../scripts/utils.js';
 
 import '../edit/da-title/da-title.js';
-import './da-media.js';
+import { daFetch } from '../shared/utils.js';
 
 const PDF_VIEWER_SRC = 'https://acrobatservices.adobe.com/view-sdk/viewer.js';
 const PDF_CLIENT_ID = 'cd73455ea6c04d0aac86270f9f5f830c';
 const PDF_DIV_ID = 'da-pdf-viewer';
 
-async function getPdfViewer(url, fileName) {
-  const { default: getStyle } = await import(`${getNx()}/utils/styles.js`);
-  const style = await getStyle(import.meta.url.replace('media.js', 'da-media.css'));
-  document.adoptedStyleSheets = [style];
-
+async function loadViewer() {
   const { default: loadScript } = await import(`${getNx()}/utils/script.js`);
   await loadScript(PDF_VIEWER_SRC);
 
-  document.addEventListener('adobe_dc_view_sdk.ready', () => {
-    const adobeDCView = new window.AdobeDC.View({ clientId: PDF_CLIENT_ID, divId: PDF_DIV_ID });
-    adobeDCView.previewFile(
-      {
-        content: { location: { url } },
-        metaData: { fileName },
-      },
-    );
+  // The window object is not instantiated
+  // fast enough after the script load event.
+  return new Promise((resolve) => {
+    const interval = setInterval(async () => {
+      if (window.AdobeDC) {
+        clearInterval(interval);
+        resolve();
+      }
+    }, 50);
   });
+}
+
+async function getPdfMedia() {
+  await loadViewer();
+
+  const { default: getStyle } = await import(`${getNx()}/utils/styles.js`);
+  const style = await getStyle('/blocks/media/da-media.css');
+  document.adoptedStyleSheets = [style];
 
   const daContent = document.createElement('div');
-  daContent.classList.add('da-pdf-content');
+  daContent.classList.add('da-pdf-media');
   daContent.id = 'da-pdf-viewer';
-
   return daContent;
+}
+
+async function getDefaultMedia() {
+  await import('./da-media.js');
+  return document.createElement('da-media');
+}
+
+async function loadPdfMedia(url, fileName) {
+  const adobeDCView = new window.AdobeDC.View({ clientId: PDF_CLIENT_ID, divId: PDF_DIV_ID });
+
+  try {
+    const resp = await daFetch(url);
+    const blob = await resp.blob();
+
+    const reader = new FileReader();
+    reader.onloadend = (e) => {
+      const promise = Promise.resolve(e.target.result);
+      adobeDCView.previewFile({ content: { promise }, metaData: { fileName } });
+    };
+    reader.readAsDataURL(blob);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 export default async function init(el) {
@@ -40,9 +67,11 @@ export default async function init(el) {
 
   const daTitle = document.createElement('da-title');
 
-  const daMedia = ext === 'pdf' ? await getPdfViewer(sourceUrl, name) : document.createElement('da-media');
+  const daMedia = ext === 'pdf' ? await getPdfMedia(el, sourceUrl, name) : await getDefaultMedia();
 
   daTitle.details = details;
   daMedia.details = details;
   el.append(daTitle, daMedia);
+
+  if (ext === 'pdf') loadPdfMedia(sourceUrl, name);
 }
