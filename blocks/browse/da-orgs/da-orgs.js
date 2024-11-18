b635ad6f8e88b039d8bcc9d974d32041811adc3a
Chris Millar <cmillar@adobe.com>
2024-11-18 16:22:01 -0700

    Ability to remove from recents

--- blocks/browse/da-orgs/da-orgs.js
+++ blocks/browse/da-orgs/da-orgs.js
@@ -44,7 +44,6 @@ export default class DaOrgs extends LitElement {
     if (recentOrgs.length === 0) return;
     this._recents = recentOrgs.map((org) => ({
       name: org,
-      load: 'eager',
       img: getRandomImg(),
     }));
   }
@@ -55,10 +54,8 @@ export default class DaOrgs extends LitElement {
     if (!resp.ok) return;
     const data = await resp.json();
     this._orgs.push(...data.reduce((acc, org) => {
-      console.log(org);
       this.updateRecentOrg(org);
       const exists = this._orgs.some((mock) => mock.name === org.name);
-      console.log(exists);
       if (!exists) {
         org.img = getRandomImg();
         acc.push(org);
@@ -72,14 +69,30 @@ export default class DaOrgs extends LitElement {
     this._full = !this._full;
   }
 
+  handleRemove(org) {
+    // Get the index of the org to remove
+    const idx = this._recents.findIndex((recent) => recent.name === org.name);
+    // Remove it from UI
+    this._recents.splice(idx, 1);
+    this.requestUpdate();
+    // Get the localstorage orgs
+    const localOrgs = JSON.parse(localStorage.getItem('da-orgs')) || [];
+    // Remove it from the local store
+    localOrgs.splice(idx, 1);
+    localStorage.setItem('da-orgs', JSON.stringify(localOrgs));
+  }
+
+  handleFlip(e, org) {
+    e.preventDefault();
+    e.stopPropagation();
+    org.flipped = !org.flipped;
+    this.requestUpdate();
+  }
+
   updateRecentOrg(org) {
     if (!this._recents) return;
-    const found = this._recents.find((recent) => {
-      console.log(recent);
-      return recent.name === org.name;
-    });
+    const found = this._recents.find((recent) => recent.name === org.name);
     if (found) found.created = org.created;
-    console.log(found);
     this.requestUpdate();
   }
 
@@ -93,50 +106,61 @@ export default class DaOrgs extends LitElement {
     return this._full ? this._orgs : this._orgs.slice(0, 2);
   }
 
-  renderOrg(org) {
+  renderOrg(org, listType) {
     return html`
-      <li>
-        <a class="da-org" href="#/${org.name}">
-          <div class="image-container">
-            <img src="${org.img}" loading="${org.load || 'lazy'}" alt="" />
-          </div>
-          <div class="details-area">
-            <p class="label">Name</p>
-            <p class="details-title">${org.name}</p>
-            ${org.created ? html`
-              <p class="label">Created</p>
-              <p class="details-title">${org.created ? this.formatDate(org.created)[0] : ''}</p>
-            ` : nothing}
-          </div>
-        </a>
+      <li class="da-org-outer">
+        <div class="da-org ${org.flipped ? 'is-flipped' : ''}">
+          <a href="#/${org.name}" class="da-org-front">
+            <div class="image-container">
+              <img src="${org.img}" loading="lazy" alt="" />
+            </div>
+            <div class="details-area">
+              <p class="label">Name</p>
+              <p class="details-title">${org.name}</p>
+              ${org.created ? html`
+                <p class="label">Created</p>
+                <p class="details-title">${org.created ? this.formatDate(org.created)[0] : ''}</p>
+              ` : nothing}
+            </div>
+          </a>
+          <button class="da-org-back" @click=${() => this.handleRemove(org)}>
+            <div class="new-icon">
+              <img src="/blocks/browse/da-orgs/img/Smock_VisibilityOff_18_N.svg" alt="Remove from recents list"/>
+            </div>
+            <p class="new-title">Remove</p>
+          </button>
+          ${listType === 'recents' ? html`
+            <button class="da-flip-btn" @click=${(e) => { this.handleFlip(e, org); }}></button>` : nothing}
+        </div>
       </li>
     `;
   }
 
-  renderOrgs(title, orgs, renderNew) {
+  renderOrgs(title, orgs) {
+    const listType = title === 'organizations' ? 'orgs' : 'recents';
     return html`
       <h1>${title}</h1>
       <ul class="da-orgs-list">
-        ${orgs.map((org) => this.renderOrg(org))}
-          ${renderNew ? html`
-            ${!this._full ? html`
-              <li>
-                <a class="da-org show-all" @click=${this.handleShowAll}>
-                  <div class="new-icon">
-                    <img src="/blocks/browse/img/Smock_More_18_N.svg" alt="See all organizations"/>
-                  </div>
-                  <p class="new-title">See more</p>
-                </a>
-              </li>` : nothing}
+        ${orgs.map((org) => this.renderOrg(org, listType))}
+        ${listType === 'orgs' ? html`
+          ${!this._full ? html`
             <li>
-              <a class="da-org new" href="/start">
+              <a class="da-org show-all" @click=${this.handleShowAll}>
                 <div class="new-icon">
-                  <img src="/blocks/browse/da-orgs/img/Smock_AddCircle_18_N.svg" alt="Add new organization"/>
+                  <img src="/blocks/browse/img/Smock_More_18_N.svg" alt="See all organizations"/>
                 </div>
-                <p class="new-title">Add new</p>
+                <p class="new-title">See more</p>
               </a>
-            </li>
-          ` : nothing}
+            </li>` : nothing}
+          <li>
+            <a class="da-org new" href="/start">
+              <div class="new-icon">
+                <img src="/blocks/browse/da-orgs/img/Smock_AddCircle_18_N.svg" alt="Add new organization"/>
+              </div>
+              <p class="new-title">Add new</p>
+            </a>
+          </li>
+        ` : nothing}
       </ul>
     `;
   }
@@ -145,8 +169,8 @@ export default class DaOrgs extends LitElement {
     return html`
       <img src="/blocks/browse/da-orgs/img/bg-gradient-org.jpg" class="da-org-bg" />
       <div class="da-org-container">
-        ${this._recents ? this.renderOrgs('Recent', this._recents) : nothing}
-        ${this._visibleOrgs ? this.renderOrgs('Organizations', this._visibleOrgs, true) : nothing}
+        ${this._recents && this._recents.length > 0 ? this.renderOrgs('recent', this._recents) : nothing}
+        ${this._visibleOrgs ? this.renderOrgs('organizations', this._visibleOrgs, true) : nothing}
       </div>
     `;
   }
