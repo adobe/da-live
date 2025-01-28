705e346300f1d7861adfdb075efffc532f38f4d6
Chris Millar <cmillar@adobe.com>
2025-01-27 23:15:00 -0700

    Remove org and site from custom editor

--- blocks/browse/shared.js
+++ blocks/browse/shared.js
@@ -2,6 +2,12 @@ export default function getEditPath({ path, ext, editor }) {
   if (ext === 'html' || ext === 'json') {
     const route = ext === 'html' ? editor : '/sheet#';
     const lastIndex = path.lastIndexOf(`.${ext}`);
+
+    if (route.includes('experience.adobe.com')) {
+      console.log(`${route}/${path.substring(0, lastIndex).split('/').slice(3).join('/')}`);
+      return `${route}/${path.substring(0, lastIndex).split('/').slice(3).join('/')}`;
+    }
+
     return `${route}${path.substring(0, lastIndex)}`;
   }
   return `/media#${path}`;
