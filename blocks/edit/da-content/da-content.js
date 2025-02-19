543e7927945107639aecbb84cc8545e6eb8275ff
Chris Millar <cmillar@adobe.com>
2025-02-18 18:28:47 -0700

    GH-349 - Open in Universal Editor
    * Exposes quiet editor button for UE
    * Only shown if UE is configured for org.
    * Inteligently determines DX org name (@sitesinternal)
    * Gracefully handles hashchange / page to page navigation
    
    Resolves: GH-349

--- blocks/edit/da-content/da-content.js
+++ blocks/edit/da-content/da-content.js
@@ -13,6 +13,7 @@ export default class DaContent extends LitElement {
     wsProvider: { attribute: false },
     _editorLoaded: { state: true },
     _versionUrl: { state: true },
+    _ueUrl: { state: true },
   };
 
   connectedCallback() {
@@ -27,10 +28,6 @@ export default class DaContent extends LitElement {
     }
   }
 
-  update(props) {
-    super.update(props);
-  }
-
   showPreview() {
     this.classList.add('show-preview');
     this.shadowRoot.querySelector('da-preview').classList.add('show-preview');
@@ -42,12 +39,27 @@ export default class DaContent extends LitElement {
     this.daVersions.classList.add('show-versions');
   }
 
-  async handleEditorLoaded() {
+  async loadViews() {
+    // Only import the web components once
     if (this._editorLoaded) return;
     const preview = import('../da-preview/da-preview.js');
     const versions = import('../da-versions/da-versions.js');
     await Promise.all([preview, versions]);
     this._editorLoaded = true;
+  }
+
+  async loadUe() {
+    const { default: ueUrlHelper } = await import('./helpers/index.js');
+    this._ueUrl = await ueUrlHelper(this.details.owner, this.details.previewUrl);
+  }
+
+  async handleEditorLoaded() {
+    this.loadViews();
+    this.loadUe();
+  }
+
+  openUe() {
+    window.location = this._ueUrl;
   }
 
   handleReset() {
@@ -87,6 +99,7 @@ export default class DaContent extends LitElement {
             </div>
             <div class="da-editor-tabs-quiet">
               <button class="da-editor-tab quiet show-versions" title="Versions" @click=${this.showVersions}>Versions</button>
+              ${this._ueUrl ? html`<button class="da-editor-tab quiet open-ue" title="Open in Universal Editor" @click=${this.openUe}>Universal Editor</button>` : nothing}
             </div>
           </div>
         ` : nothing}
