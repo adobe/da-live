2f0ccc23912259ae41cd5183136520d48874c7a4
Chris Millar <cmillar@adobe.com>
2025-02-21 08:20:31 -0700

    GH-349 - Open in Universal Editor (#350)
    * Exposes quiet editor button for UE
    * Only shown if UE is configured for org.
    * Inteligently determines DX org name (@sitesinternal)
    * Gracefully handles hashchange / page to page navigation
    
    Resolves: GH-349

---
+++ blocks/edit/da-content/helpers/index.js
@@ -0,0 +1,19 @@
+import { DA_ORIGIN } from '../../../shared/constants.js';
+import { daFetch } from '../../../shared/utils.js';
+
+export default async function getUeUrl(org, previewUrl) {
+  const UE_PREFIX = 'https://experience.adobe.com/#/{{DX_ORG}}/aem/editor/canvas/';
+  const resp = await daFetch(`${DA_ORIGIN}/config/${org}/`);
+  if (!resp.ok) return null;
+  const json = await resp.json();
+  const confSheet = json.data.data || json.data;
+  const ueConf = confSheet.find((row) => row.key === 'editor.path');
+  if (!ueConf) return null;
+  const { value } = ueConf;
+  if (!value) return null;
+  const dxOrg = value.split('/').find((split) => split.startsWith('@'));
+  if (!dxOrg) return null;
+  const prefix = UE_PREFIX.replace('{{DX_ORG}}', dxOrg);
+  const ueDomain = previewUrl.replace('https://', '').replace('.aem.live', '.ue.da.live');
+  return `${prefix}${ueDomain}`;
+}
