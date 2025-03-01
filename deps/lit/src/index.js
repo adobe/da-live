eade719558a6e0cc43c334549b9232e3a5b37b82
Chris Millar <cmillar@adobe.com>
2025-02-28 17:50:09 -0700

    Add spread helper

--- deps/lit/src/index.js
+++ deps/lit/src/index.js
@@ -2,6 +2,7 @@ import { LitElement, html, nothing, render } from 'lit';
 import { until } from 'lit/directives/until.js';
 import { createRef, ref } from 'lit/directives/ref.js';
 import { repeat } from 'lit/directives/repeat.js';
+import { spread } from '@open-wc/lit-helpers';
 
 export {
     LitElement,
@@ -11,5 +12,6 @@ export {
     ref,
     repeat,
     until,
+    spread,
     render,
 };
