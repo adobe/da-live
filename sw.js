a8066f0d2de33b93e74b052bf1fdd4290df121c3
Andrei Tuicu <tuicu@adobe.com>
2025-03-05 18:45:52 +0100

    feat: Use ServiceWorkers to fetch assets from DA Content

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
 self.addEventListener("fetch", (event) => {
   console.log(`Handling fetch event for`, event.request.url, event);
   event.respondWith(fetch(event.request));
