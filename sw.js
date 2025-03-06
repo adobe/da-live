0bd715069569855f50e2a248113434dbf3bca90b
Andrei Tuicu <tuicu@adobe.com>
2025-03-06 10:29:02 +0100

    feat: Use ServiceWorkers to fetch assets from DA Content

--- sw.js
+++ sw.js
@@ -1,15 +1,37 @@
+const DA_CONTENT_ORIGINS = [
+  'https://content.da.live',
+  'https://stage-content.da.live',
+  'https://admin.da.live', // TODO: remove this
+];
+
+const ASSETS_EXTENSIONS = [ '.jpg', '.jpeg', '.png', '.svg', '.pdf', '.gif', '.mp4', '.svg' ];
+
 self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
+let accessToken = null;
+
+function extractAccessToken(text) {
+  const params = new URLSearchParams(text);
+  return params.get('token');
+}
+
 async function handleIms(event) {
   const requestClone = event.request.clone();
   const bodyClone = await requestClone.text();
-  console.log(`IMS request`, bodyClone);
 
   const resp = await fetch(event.request);
-  const text = await resp.text();
-  console.log(`IMS response`, text);
+  const respClone = resp.clone();
+
+  if (respClone.status === 200) {
+    const json = await respClone.json();
+    if (json.valid === true && json.token?.type === 'access_token') {
+      accessToken = extractAccessToken(bodyClone);
+    }
+  }
+
+  return resp;
 }
 
 self.addEventListener("fetch", (event) => {
@@ -19,5 +41,13 @@ self.addEventListener("fetch", (event) => {
     event.respondWith(handleIms(event));
     return;
   }
+
+  if (DA_CONTENT_ORIGINS.includes(new URL(event.request.url).origin) 
+    && ASSETS_EXTENSIONS.some((ext) => pathname.endsWith(ext))
+    && accessToken
+  ) {
+    event.request.headers.set('Authorization', `Bearer ${accessToken}`);
+  }
+
   event.respondWith(fetch(event.request));
 });
