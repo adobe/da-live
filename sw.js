afcf1ba244cd3f88f9aab3d216a2450fad6c2b76
Andrei Tuicu <tuicu@adobe.com>
2025-03-06 13:19:17 +0100

    feat: Use ServiceWorkers to fetch assets from DA Content

--- sw.js
+++ sw.js
@@ -17,6 +17,10 @@ self.addEventListener('message', (event) => {
   }
 });
 
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activated');
+});
+
 self.addEventListener("fetch", (event) => {
   const url = new URL(event.request.url);
 
@@ -24,22 +28,25 @@ self.addEventListener("fetch", (event) => {
     DA_CONTENT_ORIGINS.includes(url.origin)
     && ASSETS_EXTENSIONS.some((ext) => url.pathname.endsWith(ext))
     && ['image', 'video'].includes(event.request.destination)
-    && accessToken
   ) {
-    console.log('Fetching asset with access token', accessToken);
-    console.log('Intercepted Request', event.request);
-    const headers = new Headers(event.request.headers);
-    headers.set('Authorization', `Bearer ${accessToken}`);
-    const request = new Request(
-      event.request,
-      { 
-        mode: 'cors',
-        credentials: 'omit',
-        headers,
-      },
-    );
-    event.respondWith(fetch(request));
-    return;
+    if (!accessToken) {
+      console.log('No access token found for asset', event.request.url);
+    } else {
+      console.log('Fetching asset with access token', accessToken);
+      console.log('Intercepted Request', event.request);
+      const headers = new Headers(event.request.headers);
+      headers.set('Authorization', `Bearer ${accessToken}`);
+      const request = new Request(
+        event.request,
+        { 
+          mode: 'cors',
+          credentials: 'omit',
+          headers,
+        },
+      );
+      event.respondWith(fetch(request));
+      return;
+    }
   }
 
   event.respondWith(fetch(event.request));
