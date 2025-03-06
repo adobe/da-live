7e6251490118aea2e3e2e9fd649dde8380c78a84
Andrei Tuicu <tuicu@adobe.com>
2025-03-06 10:15:54 +0100

    feat: Use ServiceWorkers to fetch assets from DA Content

--- sw.js
+++ sw.js
@@ -2,7 +2,22 @@ self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
+async function handleIms(event) {
+  const requestClone = event.request.clone();
+  const bodyClone = await requestClone.text();
+  console.log(`IMS request`, bodyClone);
+
+  const resp = await fetch(event.request);
+  const text = await resp.text();
+  console.log(`IMS response`, text);
+}
+
 self.addEventListener("fetch", (event) => {
   console.log(`Handling fetch event for`, event.request.url, event);
+
+  if (event.request.url.includes('https://ims-na1.adobelogin.com/ims/validate_token/')) {
+    event.respondWith(handleIms(event));
+    return;
+  }
   event.respondWith(fetch(event.request));
 });
