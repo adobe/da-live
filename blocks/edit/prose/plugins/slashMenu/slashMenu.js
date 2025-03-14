6459b3ba1b3d8b1e4e38736822f37038cd119137
Hannes Hertach <hanneshertach@gmail.com>
2025-03-14 16:42:25 +0100

    Slash menu table edit commands (#372)
    * allow editing blocks from slash menu
    
    * move submenu when opening new menu
    
    * add cell split/merge
    
    * update icons to match
    
    * Update blocks/edit/prose/plugins/slashMenu/slash-menu.js
    
    Co-authored-by: Chris Peyer <chrischrischris@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Chris Millar <chris@millr.org>
    Co-authored-by: Chris Peyer <chrischrischris@users.noreply.github.com>

--- blocks/edit/prose/plugins/slashMenu/slashMenu.js
+++ blocks/edit/prose/plugins/slashMenu/slashMenu.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 import { Plugin, PluginKey } from 'da-y-wrapper';
 import { getKeyAutocomplete } from './keyAutocomplete.js';
-import menuItems from './slashMenuItems.js';
+import { getDefaultItems, getTableCellItems, getTableItems } from './slashMenuItems.js';
 import './slash-menu.js';
 
 const SLASH_COMMAND_REGEX = /\/(([^/\s]+(?:\s+[^/\s]+)*)\s*([^/\s]*))?$/;
@@ -13,6 +13,8 @@ function extractArgument(title, command) {
     ? parts[parts.length - 1]
     : undefined;
 }
+
+const hasCellAreaSelected = (state) => state.selection.content().size > 0;
 
 // Get the table name if the cursor is in a table cell
 const getTableName = ($cursor) => {
@@ -38,13 +40,11 @@ const getTableName = ($cursor) => {
   const cellIndex = $cursor.index(tableCellDepth - 1);
   const row = $cursor.node(rowDepth);
 
-  // Only proceed if we're in the second column
-  if (!(row.childCount > 1 && cellIndex === 1)) return false;
-
   const firstRowContent = firstRow.child(0).textContent;
   const tableNameMatch = firstRowContent.match(/^([a-zA-Z0-9_-]+)(?:\s*\([^)]*\))?$/);
 
-  const currentRowFirstColContent = row.child(0).textContent;
+  // Only set key value if we're in the second column of a row
+  const currentRowFirstColContent = (row.childCount > 1 && cellIndex === 1) ? row.child(0).textContent : null;
 
   if (tableNameMatch) {
     return {
@@ -60,7 +60,7 @@ class SlashMenuView {
   constructor(view) {
     this.view = view;
     this.menu = document.createElement('slash-menu');
-    this.menu.items = menuItems || [];
+    this.menu.items = getDefaultItems() || [];
 
     this.menu.addEventListener('item-selected', (e) => {
       this.selectItem(e.detail);
@@ -68,21 +68,35 @@ class SlashMenuView {
 
     this.menu.addEventListener('reset-slashmenu', () => {
       // reset menu to default items
-      this.menu.items = menuItems;
+      this.menu.items = getDefaultItems();
       this.menu.left = 0;
       this.menu.top = 0;
     });
   }
 
-  updateSlashMenuItems(pluginState, $cursor) {
+  updateSlashMenuItems(pluginState, state) {
+    const { $cursor } = state.selection;
+
+    if (hasCellAreaSelected(state)) {
+      this.menu.items = getTableCellItems(state);
+      return;
+    }
+
+    if (!$cursor) {
+      this.menu.items = getDefaultItems();
+      return;
+    }
+
     const { tableName, keyValue } = getTableName($cursor);
     if (tableName) {
       const keyData = pluginState.autocompleteData?.get(tableName);
       if (keyData && keyData.get(keyValue)) {
         this.menu.items = keyData.get(keyValue);
       } else {
-        this.menu.items = menuItems;
+        this.menu.items = getTableItems(state);
       }
+    } else {
+      this.menu.items = getDefaultItems();
     }
   }
 
@@ -93,6 +107,23 @@ class SlashMenuView {
       return keyData && keyData.get(keyValue);
     }
     return false;
+  }
+
+  showMenu(command) {
+    const { state } = this.view;
+    const { $anchor } = state.selection;
+
+    this.updateSlashMenuItems(slashMenuKey.getState(state), state);
+    const coords = this.view.coordsAtPos($anchor.pos);
+
+    const viewportCoords = {
+      left: coords.left + window.pageXOffset,
+      bottom: coords.bottom + window.pageYOffset,
+    };
+
+    this.menu.show(viewportCoords);
+
+    this.menu.command = command || '';
   }
 
   update(view) {
@@ -116,17 +147,7 @@ class SlashMenuView {
 
     const match = textBefore.match(SLASH_COMMAND_REGEX);
     if (match) {
-      this.updateSlashMenuItems(slashMenuKey.getState(state), $cursor);
-      const coords = this.view.coordsAtPos($cursor.pos);
-
-      const viewportCoords = {
-        left: coords.left + window.pageXOffset,
-        bottom: coords.bottom + window.pageYOffset,
-      };
-
-      this.menu.show(viewportCoords);
-
-      this.menu.command = match[1] || '';
+      this.showMenu(match[1]);
     } else if (this.menu.visible) {
       this.menu.command = '';
       this.hide();
@@ -136,12 +157,12 @@ class SlashMenuView {
   selectItem(detail) {
     const { item } = detail;
     const { state, dispatch } = this.view;
-    const { $cursor } = state.selection;
-    if (!$cursor) return;
+    const { $anchor } = state.selection;
+    if (!$anchor) return;
 
     // Delete the slash command and any arguments
-    const deleteFrom = $cursor.pos - (this.menu.command.length + 1);
-    const deleteTo = $cursor.pos;
+    const deleteFrom = $anchor.pos - (this.menu.command.length + 1);
+    const deleteTo = $anchor.pos;
     const tr = state.tr.delete(deleteFrom, deleteTo);
     const newState = state.apply(tr);
 
@@ -196,20 +217,18 @@ export default function slashMenu() {
     },
     props: {
       handleKeyDown(editorView, event) {
+        // if multiple cells are selected, show menu without outputting a /
+        if (event.key === '/' && hasCellAreaSelected(editorView.state)) {
+          event.preventDefault();
+          pluginView.showMenu();
+          return true;
+        }
+
         if (pluginView?.menu.visible) {
           if (['ArrowUp', 'ArrowDown', 'Enter', 'Escape'].includes(event.key)) {
             event.preventDefault();
             event.stopPropagation();
-
-            if (event.key === 'Enter') {
-              const filteredItems = pluginView.menu.getFilteredItems();
-              const selectedItem = filteredItems[pluginView.menu.selectedIndex];
-              if (selectedItem) {
-                pluginView.selectItem({ item: selectedItem });
-              }
-            } else {
-              pluginView.menu.handleKeyDown(event);
-            }
+            pluginView.menu.handleKeyDown(event);
             return true;
           }
         }
