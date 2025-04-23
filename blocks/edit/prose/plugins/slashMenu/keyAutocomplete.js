9f68eec8bbe2127ff735985b6847c1fb58574318
Chris Peyer <chrischrischris@users.noreply.github.com>
2025-04-23 11:29:09 -0400

    Block Prop Lookup: Allow for uppercase in labels and values (#427)

--- blocks/edit/prose/plugins/slashMenu/keyAutocomplete.js
+++ blocks/edit/prose/plugins/slashMenu/keyAutocomplete.js
@@ -15,7 +15,7 @@ export function processKeyData(data) {
     const itemBlocks = item.blocks.toLowerCase().trim();
     const blocks = itemBlocks.split(',').map((block) => block.trim());
 
-    const values = item.values.toLowerCase().split('|').map((v) => {
+    const values = item.values.split('|').map((v) => {
       const [label, val] = v.split('=').map((vb) => vb.trim());
 
       return {
