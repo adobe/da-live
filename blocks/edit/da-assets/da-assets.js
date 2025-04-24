86d5ee464f773c8236ac578eb54bf38bb8783ff6
Sourabrata Bose <bose.sourabrata21century@gmail.com>
2025-04-24 20:47:53 +0530

    Updated fetchValue() function to handle repo level configuration fallback to org level configuration as fetched

--- blocks/edit/da-assets/da-assets.js
+++ blocks/edit/da-assets/da-assets.js
@@ -8,9 +8,14 @@ const { loadStyle } = await import(`${getNx()}/scripts/nexter.js`);
 const { loadIms, handleSignIn } = await import(`${getNx()}/utils/ims.js`);
 const loadScript = (await import(`${getNx()}/utils/script.js`)).default;
 
-const ASSET_SELECTOR_URL = 'https://experience.adobe.com/solutions/CQ-assets-selectors/assets/resources/assets-selectors.js';
+const ASSET_SELECTOR_URL =
+  'https://experience.adobe.com/solutions/CQ-assets-selectors/assets/resources/assets-selectors.js';
 
 const CONFS = {};
+
+async function getMergedConf(owner, repo) {
+  return mergedConf;
+}
 
 async function fetchConf(path) {
   if (CONFS[path]) return CONFS[path];
@@ -27,7 +32,29 @@ async function fetchConf(path) {
 async function fetchValue(path, key) {
   if (CONFS[path]?.[key]) return CONFS[path][key];
 
-  const data = await fetchConf(path);
+  const [repoConf, orgConf] = await Promise.all([
+    fetchConf(`/${owner}/${repo}/`),
+    fetchConf(`/${owner}/`),
+  ]);
+
+  const data = null;
+
+  // Start with organization-level config
+  if (orgConf) {
+    data = {};
+    orgConf.forEach((item) => {
+      data[item.key] = item.value;
+    });
+  }
+
+  // Override with repository-level config
+  if (repoConf) {
+    if (data === null) data = {};
+    repoConf.forEach((item) => {
+      data[item.key] = item.value;
+    });
+  }
+
   if (!data) return null;
 
   const confKey = data.find((conf) => conf.key === key);
@@ -39,23 +66,29 @@ async function fetchValue(path, key) {
 export async function getConfKey(owner, repo, key) {
   if (!(repo || owner)) return null;
   let value = await fetchValue(`/${owner}/${repo}/`, key);
-  if (!value) value = await fetchValue(`/${owner}/`, key);
+
+  // Turned off check for organization level value update as new fetchValue does that internally.
+  // if (!value) value = await fetchValue(`/${owner}/`, key);
+
   return value;
 }
 
 export async function openAssets() {
   const details = await loadIms();
   if (details.anonymous) handleSignIn();
-  if (!(details.accessToken)) return;
+  if (!details.accessToken) return;
 
   const { owner, repo } = getPathDetails();
   const repoId = await getConfKey(owner, repo, 'aem.repositoryId');
 
   // Determine publicly available asset origin
-  const prodOrigin = await getConfKey(owner, repo, 'aem.assets.prod.origin') || `${repoId.replace('author', 'publish')}`;
+  const prodOrigin =
+    (await getConfKey(owner, repo, 'aem.assets.prod.origin')) ||
+    `${repoId.replace('author', 'publish')}`;
 
   // Determine if images should be links
-  const injectLink = (await getConfKey(owner, repo, 'aem.assets.image.type')) === 'link';
+  const injectLink =
+    (await getConfKey(owner, repo, 'aem.assets.image.type')) === 'link';
 
   let dialog = document.querySelector('.da-dialog-asset');
   if (!dialog) {
@@ -79,7 +112,9 @@ export async function openAssets() {
       imsToken: details.accessToken.token,
       repositoryId: repoId,
       aemTierType,
-      onClose: () => { dialog.close(); },
+      onClose: () => {
+        dialog.close();
+      },
       handleSelection: (assets) => {
         const [asset] = assets;
         if (!asset) return;
@@ -91,12 +126,18 @@ export async function openAssets() {
         dialog.close();
 
         // eslint-disable-next-line no-underscore-dangle
-        const alt = asset?._embedded?.['http://ns.adobe.com/adobecloud/rel/metadata/asset']?.['dc:description'];
+        const alt =
+          asset?._embedded?.[
+            'http://ns.adobe.com/adobecloud/rel/metadata/asset'
+          ]?.['dc:description'];
 
-        const src = aemTierType === 'author'
-          ? `https://${prodOrigin}${path}`
-          // eslint-disable-next-line no-underscore-dangle
-          : asset._links['http://ns.adobe.com/adobecloud/rel/rendition'][0].href.split('?')[0];
+        const src =
+          aemTierType === 'author'
+            ? `https://${prodOrigin}${path}`
+            : // eslint-disable-next-line no-underscore-dangle
+              asset._links[
+                'http://ns.adobe.com/adobecloud/rel/rendition'
+              ][0].href.split('?')[0];
 
         const imgObj = { src, style: 'width: 180px' };
         if (alt) imgObj.alt = alt;
