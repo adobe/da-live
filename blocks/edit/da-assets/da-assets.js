e28cce044142d39012592e5d8abcfa4964766360
Ramachandra Avuthu <avutu.ramchandra@gmail.com>
2025-05-14 12:20:57 -0400

    fix: assets - Support delivery tier video links (#449)

--- blocks/edit/da-assets/da-assets.js
+++ blocks/edit/da-assets/da-assets.js
@@ -94,10 +94,18 @@ export async function openAssets() {
         // eslint-disable-next-line no-underscore-dangle
         const alt = asset?._embedded?.['http://ns.adobe.com/adobecloud/rel/metadata/asset']?.['dc:description'];
 
-        const src = aemTierType === 'author'
-          ? `https://${prodOrigin}${path}`
-          // eslint-disable-next-line no-underscore-dangle
-          : asset._links['http://ns.adobe.com/adobecloud/rel/rendition'][0].href.split('?')[0];
+        // eslint-disable-next-line no-underscore-dangle
+        const renditionLinks = asset._links['http://ns.adobe.com/adobecloud/rel/rendition'];
+        const videoLink = renditionLinks.find((link) => link.href.endsWith('/play'))?.href;
+
+        let src;
+        if (aemTierType === 'author') {
+          src = `https://${prodOrigin}${path}`;
+        } else if (mimetype.startsWith('video/')) {
+          src = videoLink;
+        } else {
+          src = renditionLinks[0]?.href.split('?')[0];
+        }
 
         const imgObj = { src, style: 'width: 180px' };
         if (alt) imgObj.alt = alt;
