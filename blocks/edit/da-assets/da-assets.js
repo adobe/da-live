c130a7c0066702539703d799d771f7ab6b54021f
Andreas Haller <andreas.haller@non.agilent.com>
2025-06-25 11:03:01 +0200

    added options for delivery via DynamicMedia to MediaBus and Author selection of SmartCropsz

--- blocks/edit/da-assets/da-assets.js
+++ blocks/edit/da-assets/da-assets.js
@@ -51,85 +51,164 @@ export async function openAssets() {
   const { owner, repo } = getPathDetails();
   const repoId = await getConfKey(owner, repo, 'aem.repositoryId');
 
-  // Determine publicly available asset origin
-  const prodOrigin = await getConfKey(owner, repo, 'aem.assets.prod.origin') || `${repoId.replace('author', 'publish')}`;
+  // Custom publicly available asset origin
+  let prodOrigin = await getConfKey(owner, repo, 'aem.assets.prod.origin');
+
+  const smartCropSelectEnabled = (await getConfKey(owner, repo, 'aem.asset.smartcrop.select')) === 'on';
+  const dmDeliveryEnabled = smartCropSelectEnabled || (await getConfKey(owner, repo, 'aem.asset.dm.delivery')) === 'on' || prodOrigin?.startsWith('delivery-');
+
+  prodOrigin = prodOrigin || `${repoId.replace('author', dmDeliveryEnabled ? 'delivery' : 'publish')}`;
+
+  const getBaseDmUrl = (asset) => `https://${prodOrigin}/adobe/assets/${asset['repo:id']}`;
+
+  const getAssetUrl = (asset, name = asset.name) => {
+    if (!dmDeliveryEnabled) {
+      return `https://${prodOrigin}${asset.path}`;
+    }
+    return `${getBaseDmUrl(asset)}/as/${name}`;
+  };
 
   // Determine if images should be links
   const injectLink = (await getConfKey(owner, repo, 'aem.assets.image.type')) === 'link';
 
   let dialog = document.querySelector('.da-dialog-asset');
-  if (!dialog) {
+  if (dialog) {
+    dialog.showModal();
+  } else {
     await loadStyle(import.meta.url.replace('.js', '.css'));
     await loadScript(ASSET_SELECTOR_URL);
 
     dialog = document.createElement('dialog');
     dialog.className = 'da-dialog-asset';
 
-    const inner = document.createElement('div');
-    inner.className = 'da-dialog-asset-inner';
+    const assetSelectorWrapper = document.createElement('div');
+    assetSelectorWrapper.className = 'da-dialog-asset-inner';
+    dialog.append(assetSelectorWrapper);
 
-    dialog.append(inner);
+    const cropSelectorWrapper = document.createElement('div');
+    cropSelectorWrapper.style.display = 'none';
+    cropSelectorWrapper.className = 'da-dialog-asset-inner';
+    dialog.append(cropSelectorWrapper);
+
+    const resetCropSelector = () => {
+      cropSelectorWrapper.style.display = 'none';
+      cropSelectorWrapper.innerHTML = '';
+      assetSelectorWrapper.style.display = 'block';
+    };
 
     const main = document.body.querySelector('main');
     main.insertAdjacentElement('afterend', dialog);
 
+    dialog.showModal();
+
     const aemTierType = repoId.includes('delivery') ? 'delivery' : 'author';
 
+    let cropSelectionShown = false;
     const selectorProps = {
       imsToken: details.accessToken.token,
       repositoryId: repoId,
       aemTierType,
-      onClose: () => { dialog.close(); },
-      handleSelection: (assets) => {
+      onClose: () => !cropSelectionShown && dialog.close(),
+      handleSelection: async (assets) => {
         const [asset] = assets;
         if (!asset) return;
         const format = asset['aem:formatName'];
         if (!format) return;
-        const { path } = asset;
         const mimetype = asset.mimetype || asset['dc:format'];
+        const isImage = mimetype?.toLowerCase().startsWith('image/');
         const { view } = window;
         const { state } = view;
-        dialog.close();
 
         // eslint-disable-next-line no-underscore-dangle
         const alt = asset?._embedded?.['http://ns.adobe.com/adobecloud/rel/metadata/embedded']?.['dc:description']
           // eslint-disable-next-line no-underscore-dangle
           || asset?._embedded?.['http://ns.adobe.com/adobecloud/rel/metadata/embedded']?.['dc:title'];
 
-        // eslint-disable-next-line no-underscore-dangle
-        const renditionLinks = asset._links['http://ns.adobe.com/adobecloud/rel/rendition'];
-        const videoLink = renditionLinks.find((link) => link.href.endsWith('/play'))?.href;
+        const createImage = (src) => {
+          const imgObj = { src, style: 'width: 180px' };
+          if (alt) imgObj.alt = alt;
+          return state.schema.nodes.image.create(imgObj);
+        };
 
-        let src;
-        if (aemTierType === 'author') {
-          src = `https://${prodOrigin}${path}`;
-        } else if (mimetype.startsWith('video/')) {
-          src = videoLink;
+        if (isImage && smartCropSelectEnabled) {
+          cropSelectionShown = true;
+          try {
+            assetSelectorWrapper.style.display = 'none';
+            cropSelectorWrapper.style.display = 'block';
+
+            const listSmartCropsResponse = await daFetch(`${getBaseDmUrl(asset)}/smartCrops`);
+            const listSmartCrops = await listSmartCropsResponse.json();
+
+            if (!(listSmartCrops.items?.length > 0)) {
+              dialog.close();
+              const fpo = createImage(getAssetUrl(asset));
+              resetCropSelector();
+              view.dispatch(state.tr.replaceSelectionWith(fpo).scrollIntoView());
+            }
+
+            cropSelectorWrapper.innerHTML = '<div class="da-dialog-asset-crops-toolbar"><button class="cancel">Cancel</button><button class="back">Back</button><button class="insert">Insert</button></div>';
+
+            const cropSelectorList = document.createElement('ul');
+            cropSelectorList.classList.add('da-dialog-asset-crops');
+            cropSelectorWrapper.append(cropSelectorList);
+
+            cropSelectorWrapper.querySelector('.cancel').addEventListener('click', () => {
+              resetCropSelector();
+              dialog.close();
+            });
+            cropSelectorWrapper.querySelector('.back').addEventListener('click', () => resetCropSelector());
+            cropSelectorWrapper.querySelector('.insert').addEventListener('click', () => {
+              dialog.close();
+              const fpo = createImage(cropSelectorList.querySelector('.selected img').src);
+              resetCropSelector();
+              view.dispatch(state.tr.replaceSelectionWith(fpo).scrollIntoView());
+            });
+
+            const cropItems = listSmartCrops.items.map((smartCrop) => `<p>${smartCrop.name}</p><img src="${getAssetUrl(asset, `${smartCrop.name}-${asset.name}`)}?smartcrop=${smartCrop.name}">`).join('</li><li>');
+            cropSelectorList.innerHTML = `<li class="selected"><p>Original</p><img src="${getAssetUrl(asset)}"></li><li>${cropItems}</li>`;
+            cropSelectorList.addEventListener('click', () => {
+              const li = cropSelectorList.querySelector('li:hover');
+              if (!li) return;
+              cropSelectorList.querySelector('.selected')?.classList.remove('selected');
+              li.classList.add('selected');
+            });
+          } finally {
+            cropSelectionShown = false;
+          }
         } else {
-          src = renditionLinks[0]?.href.split('?')[0];
+          dialog.close();
+
+          // eslint-disable-next-line no-underscore-dangle
+          const renditionLinks = asset._links['http://ns.adobe.com/adobecloud/rel/rendition'];
+          const videoLink = renditionLinks.find((link) => link.href.endsWith('/play'))?.href;
+
+          let src;
+          if (aemTierType === 'author') {
+            src = getAssetUrl(asset);
+          } else if (mimetype.startsWith('video/')) {
+            src = videoLink;
+          } else {
+            src = renditionLinks[0]?.href.split('?')[0];
+          }
+
+          let fpo;
+          // ensure assets not supported by the MediaBus are added as links
+          if (!isImage || injectLink) {
+            const para = document.createElement('p');
+            const link = document.createElement('a');
+            link.href = src;
+            link.innerText = src;
+            para.append(link);
+            fpo = proseDOMParser.fromSchema(window.view.state.schema).parse(para);
+          } else {
+            fpo = createImage(src);
+          }
+
+          view.dispatch(state.tr.replaceSelectionWith(fpo).scrollIntoView());
         }
-
-        const imgObj = { src, style: 'width: 180px' };
-        if (alt) imgObj.alt = alt;
-
-        let fpo;
-        // ensure assets not supported by the MediaBus are added as links
-        if (!mimetype || !mimetype.toLowerCase().startsWith('image/') || injectLink) {
-          const para = document.createElement('p');
-          const link = document.createElement('a');
-          link.href = src;
-          link.innerText = src;
-          para.append(link);
-          fpo = proseDOMParser.fromSchema(window.view.state.schema).parse(para);
-        } else {
-          fpo = state.schema.nodes.image.create(imgObj);
-        }
-
-        view.dispatch(state.tr.replaceSelectionWith(fpo).scrollIntoView());
       },
     };
-    window.PureJSSelectors.renderAssetSelector(inner, selectorProps);
-  }
 
-  dialog.showModal();
+    window.PureJSSelectors.renderAssetSelector(assetSelectorWrapper, selectorProps);
+  }
 }
