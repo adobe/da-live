a4afe8c535a0ee4f63b463d9e0ac6fc0affe723e
Hannes Hertach <hanneshertach@gmail.com>
2025-07-07 12:08:20 +0200

    show sites instead of orgs on homepage

--- blocks/browse/da-orgs/da-orgs.js
+++ blocks/browse/da-orgs/da-orgs.js
@@ -21,9 +21,17 @@ export default class DaOrgs extends LitElement {
     this.getRecents();
   }
 
-  getRecents() {
-    const recentOrgs = JSON.parse(localStorage.getItem('da-orgs')) || [];
-    if (recentOrgs.length === 0) return;
+  mapRecentSites(recentSites) {
+    this._recents = recentSites.map((name) => (
+      {
+        name,
+        img: `/blocks/browse/da-orgs/img/cards/da-${getRandom()}.jpg`,
+        style: `da-card-style-${getRandom()}`,
+      }
+    ));
+  }
+
+  mapRecentOrgs(recentOrgs) {
     this._recents = recentOrgs.map((name) => (
       {
         name,
@@ -31,6 +39,18 @@ export default class DaOrgs extends LitElement {
         style: `da-card-style-${getRandom()}`,
       }
     ));
+  }
+
+  getRecents() {
+    const recentSites = JSON.parse(localStorage.getItem('da-sites')) || [];
+    const recentOrgs = JSON.parse(localStorage.getItem('da-orgs')) || [];
+
+    if (recentSites.length > 0) {
+      this.mapRecentSites(recentSites);
+      localStorage.removeItem('da-orgs');
+    } else if (recentOrgs.length > 0) {
+      this.mapRecentOrgs(recentOrgs);
+    }
   }
 
   setStatus(text, description, type = 'info') {
@@ -44,10 +64,10 @@ export default class DaOrgs extends LitElement {
     this._recents.splice(idx, 1);
     this.requestUpdate();
     // Get the localstorage orgs
-    const localOrgs = JSON.parse(localStorage.getItem('da-orgs')) || [];
+    const localOrgs = JSON.parse(localStorage.getItem('da-sites')) || [];
     // Remove it from the local store
     localOrgs.splice(idx, 1);
-    localStorage.setItem('da-orgs', JSON.stringify(localOrgs));
+    localStorage.setItem('da-sites', JSON.stringify(localOrgs));
   }
 
   handleFlip(e, org) {
@@ -60,9 +80,18 @@ export default class DaOrgs extends LitElement {
   async handleGo(e) {
     e.preventDefault();
     const formData = new FormData(e.target);
-    const { org } = Object.fromEntries(formData);
-    if (!org) return;
-    window.location = `#/${org}`;
+    const { siteUrl } = Object.fromEntries(formData);
+    if (!siteUrl) return;
+    try {
+      const url = new URL(siteUrl);
+      const helixString = url.hostname.split('.')[0];
+      // eslint-disable-next-line no-unused-vars
+      const [_, repo, org] = helixString.split('--');
+      if (!repo || !org) return;
+      window.location = `#/${org}/${repo}`;
+    } catch (_) {
+      // empty
+    }
   }
 
   handleShare(org) {
@@ -87,9 +116,9 @@ export default class DaOrgs extends LitElement {
   renderGo() {
     return html`
       <form @submit=${this.handleGo}>
-        <input type="text" name="org" placeholder="organization" />
+        <input type="text" name="siteUrl" placeholder="https://main--site--org.aem.live" />
         <div class="da-form-btn-offset">
-          <button aria-label="Go to organization">
+          <button aria-label="Go to site">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 26">
               <path fill="currentColor"
                 d="M23.09,13.67c.14-.35.14-.74,0-1.08-.07-.17-.18-.33-.31-.46l-6.62-6.62c-.55-.55-1.45-.55-2,0-.55.55-.55,1.45,0,2l4.21,4.21H4.61c-.78,0-1.41.63-1.41,1.42s.63,1.42,1.41,1.42h13.76l-4.21,4.21c-.55.55-.55,1.45,0,2,.28.28.64.41,1,.41s.72-.14,1-.41l6.62-6.62c.13-.13.23-.29.31-.46Z" />
@@ -100,7 +129,7 @@ export default class DaOrgs extends LitElement {
     `;
   }
 
-  renderOrg(org) {
+  renderSite(org) {
     return html`
       <li class="da-org-outer">
         <div class="da-org ${org.flipped ? 'is-flipped' : ''}">
@@ -110,7 +139,8 @@ export default class DaOrgs extends LitElement {
             </picture>
             <div class="bg-overlay ${org.style}">
               <a href="#/${org.name}">
-                <span>${org.name}</span>
+                <span>${org.name.split('/')[1]}</span>
+                <span>${org.name.split('/')[0]}</span>
                 <span class="da-org-card-action da-org-card-action-go">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
                       <path fill="currentColor" d="M22.91,16.38c.1-.24.1-.51,0-.76-.05-.12-.12-.23-.21-.32l-4.63-4.63c-.39-.39-1.01-.39-1.4,0-.39.39-.39,1.01,0,1.4l2.94,2.94h-9.62c-.55,0-.99.44-.99.99s.44.99.99.99h9.62l-2.94,2.94c-.39.39-.39,1.01,0,1.4.19.19.45.29.7.29s.51-.1.7-.29l4.63-4.63c.09-.09.16-.2.21-.32Z" />
@@ -141,20 +171,19 @@ export default class DaOrgs extends LitElement {
     `;
   }
 
-  renderOrgs(title, orgs) {
-    const listType = title === 'organizations' ? 'orgs' : 'recents';
+  renderSites(orgs) {
     return html`
-      <ul class="da-orgs-list">${orgs.map((org) => this.renderOrg(org, listType))}</ul>
+      <ul class="da-orgs-list">${orgs.map((org) => this.renderSite(org))}</ul>
     `;
   }
 
   renderEmpty() {
     return html`
       <div class="da-no-org-well">
-        <img src="/blocks/browse/da-orgs/img/org-icon-color.svg" width="80" height="60" alt=""/>
+        <img src="/blocks/browse/da-orgs/img/site-icon-color.svg" width="78" height="60" alt=""/>
         <div class="da-no-org-text">
-          <h3>You don’t have any recent organizations.</h3>
-          <p>Go to your organization, play in the sandbox, or create a new one.</p>
+          <h3>You don’t have any recent sites.</h3>
+          <p>Enter the URL for your site here to get started.</p>
         </div>
         ${this.renderGo()}
       </div>
@@ -168,9 +197,9 @@ export default class DaOrgs extends LitElement {
         <div class="da-org-header">
           <h2>Recents</h2>
         </div>
-        ${this._recents && this._recents.length > 0 ? this.renderOrgs('recent', this._recents) : this.renderEmpty()}
+        ${this._recents && this._recents.length > 0 ? this.renderSites(this._recents) : this.renderEmpty()}
         <div class="da-org-header">
-          <h2>Organizations</h2>
+          <h2>Sites</h2>
           ${this._recents && this._recents.length > 0 ? this.renderGo() : nothing}
         </div>
         <div class="da-org-sandbox-new">
