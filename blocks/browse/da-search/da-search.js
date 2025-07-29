cb52964abff02a3493b604541bb1fcde85a22055
Chris Millar <cmillar@adobe.com>
2025-07-29 11:36:26 -0600

    Add SVGs to search

--- blocks/browse/da-search/da-search.js
+++ blocks/browse/da-search/da-search.js
@@ -55,7 +55,7 @@ export default class DaSearch extends LitElement {
   }
 
   async getMatches(startPath, term) {
-    const searchTypes = ['.html', '.json'];
+    const searchTypes = ['.html', '.json', '.svg'];
 
     const searchFile = async (file, prevRetry = 0) => {
       if (!searchTypes.some((type) => file.path.endsWith(type))) return;
