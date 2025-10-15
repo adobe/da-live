5c2374b2bc058ab4fe367bc60485f0523fbef11e
Chris Peyer <chrischrischris@users.noreply.github.com>
2025-10-15 15:07:19 -0400

    Support New Regional Diff Format (#595)
    * Support New Regional Diff Format
    
    Fixes https://github.com/adobe/da-live/issues/500
    
    * Rename loc to diff
    
    * Fix test selectors
    
    * Move bg color to css
    
    * Fix undo when accepting a block

--- test/e2e/tests/regionaledits.spec.js
+++ test/e2e/tests/regionaledits.spec.js
@@ -43,20 +43,20 @@ test('Regional Edit Document', async ({ page, context }, workerInfo) => {
   await page.getByRole('link', { name: 'regionaledit', exact: true }).click();
 
   const newPage = await findPageTab('Edit regionaledit', page, context);
-  await expect(newPage.locator('div.loc-color-overlay.loc-langstore')).toBeVisible();
-  await expect(newPage.locator('div.loc-color-overlay.loc-regional')).toBeVisible();
+  await expect(newPage.locator('div.diff-color-overlay.diff-upstream')).toBeVisible();
+  await expect(newPage.locator('div.diff-color-overlay.diff-local')).toBeVisible();
 
   await expect(newPage.getByText('Deleted H1 Here', { exact: true })).toBeVisible();
   await expect(newPage.getByText('Added H1 Here', { exact: true })).toBeVisible();
 
-  await newPage.locator('div.loc-color-overlay.loc-langstore').hover();
-  await newPage.locator('da-loc-deleted').getByText('Delete', { exact: true }).click();
+  await newPage.locator('div.diff-color-overlay.diff-upstream').hover();
+  await newPage.locator('da-diff-deleted').getByText('Delete', { exact: true }).click();
   await expect(newPage.getByText('Deleted H1 Here', { exact: true })).not.toBeVisible();
 
-  await newPage.locator('div.loc-color-overlay.loc-regional').hover();
-  await newPage.locator('da-loc-added').getByText('Keep', { exact: true }).click();
+  await newPage.locator('div.diff-color-overlay.diff-local').hover();
+  await newPage.locator('da-diff-added').getByText('Keep', { exact: true }).click();
   await expect(newPage.getByText('Added H1 Here', { exact: true })).toBeVisible();
-  await expect(newPage.locator('div.loc-color-overlay.loc-regional')).not.toBeVisible();
+  await expect(newPage.locator('div.diff-color-overlay.diff-local')).not.toBeVisible();
 
   // Note that the test folder will be automatically cleaned up in subsequent runs
   // by the delete.spec.js test
