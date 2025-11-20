b6d5135bd37d36c4a1cb09c34b81df042b609433
Hannes Hertach <hanneshertach@gmail.com>
2025-11-20 15:40:45 +0100

    add base class

---
+++ blocks/edit/prose/plugins/inContextMenu.js
@@ -0,0 +1,163 @@
+import { LitElement, html } from 'da-lit';
+
+/**
+ * Base class for context menus in the editor (slash menu, link menu, etc.)
+ * Provides common functionality for positioning, keyboard navigation, and item selection
+ */
+export default class InContextMenu extends LitElement {
+  static properties = {
+    items: { type: Array },
+    selectedIndex: { type: Number, reflect: true },
+    visible: { type: Boolean, reflect: true },
+    left: { type: Number },
+    top: { type: Number },
+  };
+
+  constructor() {
+    super();
+    this.items = [];
+    this.selectedIndex = 0;
+    this.visible = false;
+    this.left = 0;
+    this.top = 0;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    // Subclasses should set adoptedStyleSheets in their connectedCallback
+  }
+
+  show(coords) {
+    this.visible = true;
+    this.left = coords.left;
+    this.top = coords.top || (coords.bottom + 5);
+    this.requestUpdate();
+  }
+
+  hide() {
+    this.visible = false;
+    this.selectedIndex = 0;
+  }
+
+  updatePosition() {
+    const { left, top } = this;
+    const rect = this.getBoundingClientRect();
+    const viewportWidth = window.innerWidth;
+    const viewportHeight = window.innerHeight;
+
+    let adjustedLeft = left;
+    let adjustedTop = top;
+
+    // Adjust horizontal position if menu would overflow viewport
+    if (adjustedLeft + rect.width > viewportWidth) {
+      adjustedLeft = viewportWidth - rect.width - 10;
+    }
+
+    // Adjust vertical position if menu would overflow viewport
+    if (adjustedTop + rect.height > viewportHeight) {
+      adjustedTop = top - rect.height - 20;
+    }
+
+    // Ensure menu doesn't go off-screen to the left
+    if (adjustedLeft < 0) {
+      adjustedLeft = 10;
+    }
+
+    // Ensure menu doesn't go above viewport
+    if (adjustedTop < 0) {
+      adjustedTop = 10;
+    }
+
+    this.style.left = `${adjustedLeft}px`;
+    this.style.top = `${adjustedTop}px`;
+  }
+
+  updated(changedProperties) {
+    if (changedProperties.has('left') || changedProperties.has('top')) {
+      this.updatePosition();
+    }
+  }
+
+  handleItemClick(item) {
+    this.dispatchEvent(new CustomEvent('item-selected', { detail: { item } }));
+    this.hide();
+  }
+
+  /**
+   * Get the items to display - can be overridden by subclasses for filtering
+   */
+  getDisplayItems() {
+    return this.items;
+  }
+
+  next() {
+    const displayItems = this.getDisplayItems();
+    const newIndex = (this.selectedIndex + 1) % displayItems.length;
+    this.selectedIndex = newIndex;
+    this.scrollSelectedIntoView();
+    this.requestUpdate();
+  }
+
+  previous() {
+    const displayItems = this.getDisplayItems();
+    const len = displayItems.length;
+    const newIndex = (this.selectedIndex - 1 + len) % len;
+    this.selectedIndex = newIndex;
+    this.scrollSelectedIntoView();
+    this.requestUpdate();
+  }
+
+  handleKeyDown(event) {
+    if (!this.visible) return;
+
+    const displayItems = this.getDisplayItems();
+    if (!displayItems.length) return;
+
+    switch (event.key) {
+      case 'ArrowDown': {
+        event.preventDefault();
+        this.next();
+        break;
+      }
+      case 'ArrowUp': {
+        event.preventDefault();
+        this.previous();
+        break;
+      }
+      case 'Enter': {
+        event.preventDefault();
+        if (displayItems[this.selectedIndex]) {
+          const selectedItem = displayItems[this.selectedIndex];
+          this.handleItemClick({ ...selectedItem });
+        }
+        this.requestUpdate();
+        break;
+      }
+      case 'Escape': {
+        event.preventDefault();
+        this.hide();
+        this.requestUpdate();
+        break;
+      }
+      default:
+        break;
+    }
+  }
+
+  scrollSelectedIntoView() {
+    this.updateComplete.then(() => {
+      const selectedItem = this.shadowRoot.querySelector('.menu-item.selected');
+      if (selectedItem) {
+        selectedItem.scrollIntoView({ block: 'nearest' });
+      }
+    });
+  }
+
+  /**
+   * Subclasses must implement render()
+   */
+  render() {
+    return html``;
+  }
+}
+
