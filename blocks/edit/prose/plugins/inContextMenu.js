0786ad1b349763152b7018f238101039dc6c2d0d
Hannes Hertach <hanneshertach@gmail.com>
2025-12-04 17:59:23 +0100

    change fixed to absolute

--- blocks/edit/prose/plugins/inContextMenu.js
+++ blocks/edit/prose/plugins/inContextMenu.js
@@ -21,19 +21,10 @@ export default class InContextMenu extends LitElement {
 
   connectedCallback() {
     super.connectedCallback();
-    this.handleScroll = this.handleScroll.bind(this);
-    window.addEventListener('scroll', this.handleScroll, true);
   }
 
   disconnectedCallback() {
     super.disconnectedCallback();
-    window.removeEventListener('scroll', this.handleScroll, true);
-  }
-
-  handleScroll() {
-    if (this.visible) {
-      this.hide();
-    }
   }
 
   show(coords) {
@@ -51,20 +42,27 @@ export default class InContextMenu extends LitElement {
   updatePosition() {
     const { left, top } = this;
     const rect = this.getBoundingClientRect();
-    const viewportWidth = window.innerWidth;
-    const viewportHeight = window.innerHeight;
+    
+    // Find the parent ProseMirror element
+    const proseMirrorEl = this.closest('.da-prose-mirror');
+    if (!proseMirrorEl) return;
+    
+    const parentRect = proseMirrorEl.getBoundingClientRect();
+    const parentWidth = parentRect.width;
+    const parentHeight = parentRect.height;
 
-    let adjustedLeft = left;
-    let adjustedTop = top;
+    // Convert viewport coordinates to parent-relative coordinates
+    let adjustedLeft = left - parentRect.left;
+    let adjustedTop = top - parentRect.top;
 
-    // Adjust horizontal position if menu would overflow viewport
-    if (adjustedLeft + rect.width > viewportWidth) {
-      adjustedLeft = viewportWidth - rect.width - 10;
+    // Adjust horizontal position if menu would overflow parent
+    if (adjustedLeft + rect.width > parentWidth) {
+      adjustedLeft = parentWidth - rect.width - 10;
     }
 
-    // Adjust vertical position if menu would overflow viewport
-    if (adjustedTop + rect.height > viewportHeight) {
-      adjustedTop = top - rect.height - 20;
+    // Adjust vertical position if menu would overflow parent
+    if (adjustedTop + rect.height > parentHeight) {
+      adjustedTop = adjustedTop - rect.height - 20;
     }
 
     // Ensure menu doesn't go off-screen to the left
@@ -72,7 +70,7 @@ export default class InContextMenu extends LitElement {
       adjustedLeft = 10;
     }
 
-    // Ensure menu doesn't go above viewport
+    // Ensure menu doesn't go above parent
     if (adjustedTop < 0) {
       adjustedTop = 10;
     }
