44bf1b470e25d411312a8548588d339f85effbb0
rofe <github@rofe.com>
2025-12-22 17:28:59 +0100

    chore: inline

--- blocks/start/start.js
+++ blocks/start/start.js
@@ -3,7 +3,6 @@ import { getDaAdmin } from '../shared/constants.js';
 import getSheet from '../shared/sheet.js';
 import { daFetch } from '../shared/utils.js';
 import { copyConfig, copyContent, previewContent } from './index.js';
-import { loadConfig, saveConfig } from './utils.js';
 
 const sheet = await getSheet('/blocks/start/start.css');
 
@@ -32,6 +31,79 @@ const AEM_TEMPLATES = [
     content: '/da-sites/author-kit-starter',
   },
 ];
+
+const DEF_CONFIG = `{
+    "data": {
+        "total": 1,
+        "limit": 1,
+        "offset": 0,
+        "data": [{}]
+    },
+    "permissions": {
+        "total": 2,
+        "limit": 2,
+        "offset": 0,
+        "data": [
+          {
+              "path": "CONFIG",
+              "groups": "{{EMAIL}}",
+              "actions": "write",
+              "comments": "The ability to set configurations for an org."
+          },
+          {
+              "path": "/ + **",
+              "groups": "{{EMAIL}}",
+              "actions": "write",
+              "comments": "The ability to create content."
+          }
+        ]
+    },
+    ":names": [
+        "data",
+        "permissions"
+    ],
+    ":version": 3,
+    ":type": "multi-sheet"
+}`;
+
+async function fetchConfig(org, body) {
+  let opts;
+  if (body) opts = { method: 'POST', body };
+
+  return daFetch(`${DA_ORIGIN}/config/${org}/`, opts);
+}
+
+export async function loadConfig(org) {
+  const resp = await fetchConfig(org);
+
+  const result = { status: resp.status };
+
+  if (!resp.ok) {
+    if (resp.status === 403 && resp.status === 401) {
+      result.message = 'You are not authorized to change this organization.';
+    }
+  } else {
+    const json = await resp.json();
+    if (json) result.json = json;
+  }
+
+  return result;
+}
+
+export async function saveConfig(org, email, existingConfig) {
+  const defConfigStr = DEF_CONFIG.replaceAll('{{EMAIL}}', email);
+  const defConfig = JSON.parse(defConfigStr);
+
+  // Preserve the existing config
+  if (existingConfig?.data) defConfig.data = existingConfig;
+
+  const body = new FormData();
+  body.append('config', JSON.stringify(defConfig));
+
+  const resp = await fetchConfig(org, body);
+
+  return { status: resp.status };
+}
 
 class DaStart extends LitElement {
   static properties = {
