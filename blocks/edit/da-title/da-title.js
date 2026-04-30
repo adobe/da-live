e4dfb1523b2fe82c53670e2fd5a3a8fac97f0f6c
Adrian Kozma <kozma@adobe.com>
2026-04-30 11:22:31 +0200

    fix: Create versions for forms.

--- blocks/edit/da-title/da-title.js
+++ blocks/edit/da-title/da-title.js
@@ -311,14 +311,11 @@ export default class DaTitle extends LitElement {
       window.open(toOpen, toOpen);
     }
 
-    if (view === 'edit') {
+    if (view === 'edit' || view === 'sheet' || view === 'form') {
       if (action === 'publish') saveDaVersion(fullpath, 'Published');
       else if (action === 'preview') saveDaVersion(fullpath, 'Previewed');
     }
-    if (view === 'sheet') {
-      if (action === 'publish') saveDaVersion(fullpath, 'Published');
-      else if (action === 'preview') saveDaVersion(fullpath, 'Previewed');
-    }
+
     this._isSending = false;
   }
 
