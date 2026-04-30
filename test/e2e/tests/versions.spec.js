f41cd45b5f51c22a641c6c454f9d4e56f55dd62e
Chris Peyer <chrischrischris@users.noreply.github.com>
2026-04-30 16:00:07 -0400

    fix: e2e tests
    Also update collab status at load.

--- test/e2e/tests/versions.spec.js
+++ test/e2e/tests/versions.spec.js
@@ -18,6 +18,7 @@ test('Create Version and Restore from it', async ({ page }, workerInfo) => {
   test.setTimeout(60000);
 
   await page.goto(getTestPageURL('versions', workerInfo));
+  await page.getByText('Create document', { exact: true }).click();
   await expect(page.locator('div.ProseMirror')).toBeVisible();
   await expect(page.locator('div.ProseMirror')).toHaveAttribute('contenteditable', 'true');
   // Allow Y.js WebSocket to stabilize before typing
