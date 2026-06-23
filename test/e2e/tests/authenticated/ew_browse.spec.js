943d02a7e765a1b9c399baf066b64ef531b8729f
svinod <svinod@adobe.com>
2026-06-23 13:36:28 +0200

    chore: add required setup

--- test/e2e/tests/authenticated/ew_browse.spec.js
+++ test/e2e/tests/authenticated/ew_browse.spec.js
@@ -2,8 +2,15 @@ import { test, expect } from '@playwright/test';
 import ENV from '../../utils/env.js';
 import { getQuery } from '../../utils/page.js';
 
+/*
+ * Requires the following config at https://da.live/config#/da-testautomation/:
+ *   path                    groups                      actions
+ *   /ewtest/+**             907136ED5D35CBF50A495CD4    read
+ * And a document at /da-testautomation/ewtest/demo with EW enabled on the site.
+ */
+
 const EW_SITE = `${ENV}/${getQuery()}#/da-testautomation/ewtest`;
-const NON_EW_SITE = `${ENV}/${getQuery()}#/da-testautomation/acltest`;
+const NON_EW_SITE = `${ENV}/${getQuery()}#/da-testautomation/acltest/testdocs/subdir`;
 
 test('Chat button is visible on EW-enabled site', async ({ page }) => {
   await page.goto(EW_SITE);
