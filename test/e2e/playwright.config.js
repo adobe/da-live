9203f43803fecd3d085f96c74702bf5479910540
Markus Haack <mhaack@adobe.com>
2026-07-16 14:59:31 +0200

    fix: restore test config

--- test/e2e/playwright.config.js
+++ test/e2e/playwright.config.js
@@ -19,7 +19,7 @@ module.exports = defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 3 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 1,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
