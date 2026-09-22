38d92150d0274b000bcd32e8af6b714285effad5
Ben Peter <benp@adobe.com>
2026-09-22 16:37:37 +0200

    fix(ew): refresh extensions with page context

--- blocks/canvas/ew-tool-panel/tool-panel.js
+++ blocks/canvas/ew-tool-panel/tool-panel.js
@@ -26,10 +26,13 @@ class EwToolPanel extends LitElement {
     activeId: { type: String },
     org: { type: String },
     site: { type: String },
+    contextKey: { type: String },
     _fullsizeDialogViewId: { state: true },
   };
 
   _loaded = {};
+
+  _loadedKeys = {};
 
   connectedCallback() {
     super.connectedCallback();
@@ -101,16 +104,29 @@ class EwToolPanel extends LitElement {
   }
 
   _pruneLoadedViews() {
-    const ids = new Set(this.views.map((v) => v.id));
+    const viewsById = new Map(this.views.map((v) => [v.id, v]));
     Object.keys(this._loaded).forEach((id) => {
-      if (!ids.has(id)) {
+      const view = viewsById.get(id);
+      if (!view || (view.cacheKey !== undefined && view.cacheKey !== this._loadedKeys[id])) {
         this._loaded[id].remove();
         delete this._loaded[id];
+        delete this._loadedKeys[id];
+      }
+    });
+  }
+
+  _clearPageBoundViews() {
+    Object.keys(this._loaded).forEach((id) => {
+      if (this._loadedKeys[id] !== undefined) {
+        this._loaded[id].remove();
+        delete this._loaded[id];
+        delete this._loadedKeys[id];
       }
     });
   }
 
   async updated(changed) {
+    if (changed.has('contextKey')) this._clearPageBoundViews();
     if (changed.has('views')) await this._onViewsChange();
     if (changed.has('activeId')) {
       if (this.activeId) persistToolPanelView(this.activeId);
@@ -131,6 +147,7 @@ class EwToolPanel extends LitElement {
       this._closeDialog();
       this.activeId = undefined;
       this._loaded = {};
+      this._loadedKeys = {};
       this.shadowRoot.querySelector('.tool-panel-content').replaceChildren();
       return;
     }
@@ -151,6 +168,9 @@ class EwToolPanel extends LitElement {
           availableIds: ids,
         });
       await this.showPanel(initial ?? this.views[0].id);
+    } else if (!this._loaded[this.activeId]) {
+      await this.showPanel(this.activeId);
+      this._syncContent();
     }
   }
 
@@ -197,6 +217,7 @@ class EwToolPanel extends LitElement {
     }
     if (!this._loaded[name]) {
       this._loaded[name] = await consumer.load();
+      this._loadedKeys[name] = consumer.cacheKey;
     }
     this.activeId = name;
   }
